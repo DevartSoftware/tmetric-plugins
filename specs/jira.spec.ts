7b8c5555fa956cae6923ecdce8fcfea15388e980
AlexeyN <alexeyn@devart.com>
2015-09-15 12:57:08 +0300

    #62246 - Parser test. Working one.

--- specs/jira.spec.ts
+++ specs/jira.spec.ts
@@ -1,51 +1,29 @@
 ﻿/// <reference path="../typings/jasmine/jasmine" />
+/// <reference path="../typings/jquery/jquery" />
 /// <reference path="../in-page-scripts/IntegrationService" />
 
-describe("Extension correctly parses", () =>
-{
-    describe("Jira", () =>
-    {
-        it("standalone task page", () =>
-        {
-            // https://testdevart1.atlassian.net/browse/TT-1
-            addTemplateToBrowser('testJira.html');
+describe("Extension correctly parses Jira", () => {
+    it("standalone task page", () => {
+        // https://testdevart1.atlassian.net/browse/TT-1
+        addTemplateToBrowser('jiraBrowseTask.html');
 
-            var result = Integrations.IntegrationService.parsePage();
-            expect(result.length).toBe(1);
-            console.log(result[0]);
-            expect(result[0]).toBe({
-                issueId: 'TT-1',
-                issueName: 'Create integration with Jira',
-                issueUrl: '/browse/TT-1',
-                projectName: 'Time Tracker',
-                serviceUrl: 'http://localhost:9876',
-                serviceType: 'Jira'
-            });
-        });
+        var result = Integrations.IntegrationService.parsePage();
+        expect(result.length).toBe(1);
 
-        xit("standalone task page", () =>
-        {
-            // https://testdevart1.atlassian.net/servicedesk/agent/DESK/queues/1/DESK-1
-            addTemplateToBrowser('jiraServiceDesk.html');
-
-            var result = Integrations.IntegrationService.parsePage();
-            expect(result.length).toBe(1);
-            //expect(result[0]).toBe({
-            //    issueId: 'TT-1',
-            //    issueName: 'Create integration with Jira',
-            //    issueUrl: '/browse/TT-1',
-            //    projectName: 'Time Tracker',
-            //    serviceUrl: 'http://localhost:9876',
-            //    serviceType: 'Jira'
-            //});
+        expect(result[0]).toEqual({
+            issueId: 'TT-1',
+            issueName: 'Create integration with Jira',
+            issueUrl: '/browse/TT-1',
+            projectName: 'Time Tracker',
+            serviceUrl: 'http://localhost:9876', // change it after mocking URl
+            serviceType: 'Jira'
         });
     });
 });
 
 
-var addTemplateToBrowser = (templateName: string) =>
-{
-    var pathToTemplate = 'base/spec/templates/' + templateName;
+var addTemplateToBrowser = (templateName: string) => {
+    var pathToTemplate = 'base/specs/templates/' + templateName;
 
     var file = $.ajax(pathToTemplate, { async: false, type: 'GET' });
 
