92b7abe49a3dab301377ba253f66fd3b6274ddf2
SergeyMul <sergeymul@devart.com>
2015-10-06 11:52:26 +0300

    address merge request comments

--- in-page-scripts/integrations/GitLab.ts
+++ in-page-scripts/integrations/GitLab.ts
@@ -13,15 +13,13 @@ module Integrations {
         match(source: Source): boolean {
             var descriptionMeta = $$('meta[name=description]');
             if (descriptionMeta) {
-                return descriptionMeta.getAttribute('content').indexOf('GitLab') > -1;
+                return descriptionMeta.getAttribute('content').indexOf('GitLab') >= 0;
             }
             return false;
         }
 
         render(issueElement: HTMLElement, linkElement: HTMLElement) {
-            var issueHost = $$('.issue .issue-details .page-title .pull-right');
-            var mergeRequestHost = $$('.merge-request .merge-request-details .page-title .pull-right');
-            var host = issueHost || mergeRequestHost;
+            var host = $$('.page-title .pull-right') || $$('.page-title');
             if (host) {
                 linkElement.classList.add('gitlab');
                 linkElement.classList.add('btn');
@@ -32,74 +30,46 @@ module Integrations {
 
         getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
 
-            // https://gitlab.com/USER/PROJECT/issues/NUMBER
-            // https://gitlab.com/USER/PROJECT/merge_requests/NUMBER
+            // https://gitlab.com/NAMESPACE/PROJECT/issues/NUMBER
+            // https://gitlab.com/NAMESPACE/PROJECT/merge_requests/NUMBER
 
-            var issuesMatch = /^(.+)\/issues\/(\d+)$/.exec(source.path);
-            var mergeRequestsMatch = /^(.+)\/merge_requests\/(\d+)$/.exec(source.path);
-            var match;
+            var match = /^(.+)\/(issues|merge_requests)\/(\d+)$/.exec(source.path);
 
-            var isIssuesPath = issuesMatch !== null;
-            var isMergeRequestsPath = mergeRequestsMatch !== null;
-            
             var result;
 
-            if (isIssuesPath || isMergeRequestsPath) {
+            if (match) {
 
                 var issueId, issueName, projectName, serviceType, serviceUrl, issueUrl;
                 
-                if (isIssuesPath) {
-                    
-                    match = issuesMatch;
-                    
-                    // <h2 class="issue-title">IssueName</h2>
-                    issueName = $$('.content .issue-details .issue-title', true).textContent;
-
-                } else if (isMergeRequestsPath) {
-                    
-                    match = mergeRequestsMatch;
-                    
-                    // <h2 class="issue-title">IssueName</h2>
-                    issueName = $$('.content .merge-request-details .issue-title', true).textContent;
-                    
-                }
-
-                // match[2] is a 'NUMBER' from path
-                issueId = match[2];
-
-                if (!issueId || !issueName) {
+                // match[3] is a 'NUMBER' from path
+                issueId = match[3];
+                if (!issueId) {
                     return;
                 }
-
                 issueId = '#' + issueId;
 
+                // <h2 class="issue-title">IssueName</h2>
+                issueName = $$('.issue-title').textContent;
+                if (!issueName) {
+                    return;
+                }
                 issueName = issueName.trim();
 
                 // <h1 class="title">
                 //      <span>
-                //          <a href="/u/USER">UserName</a> /
-                //          <a href="/USER/PROJECT">ProjectName</a> ·
-                //          <a href="/USER/PROJECT/issues">Issues</a>
-                //      </span>
-                //  </h1>
-                //
-                // or
-                //
-                // <h1 class="title">
-                //      <span>
-                //          <a href="/u/USER">USER</a> /
-                //          <a href="/USER/PROJECT">PROJECT</a> ·
-                //          <a href="/USER/PROJECT/merge_requests">Merge Requests</a>
+                //          <a href="/(u|groups)/NAMESPACE">NameSpace</a> /
+                //          <a href="/NAMESPACE/PROJECT">ProjectName</a> ·
+                //          <a href="/NAMESPACE/PROJECT/(issues|merge_requests)">(Issues|Merge Requests)</a>
                 //      </span>
                 //  </h1>
 
-                projectName = $$('header .header-content .title a:nth-last-child(2)', true).textContent;
+                projectName = $$('.title a:nth-last-child(2)').textContent;
                 projectName = projectName.trim();
 
                 serviceType = 'GitLab';
 
-                // match[1] is a 'https://gitlab.com/USER/PROJECT' from path
-                // cut '/USER/PROJECT' from path
+                // match[1] is a 'https://gitlab.com/NAMESPACE/PROJECT' from path
+                // cut '/NAMESPACE/PROJECT' from path
                 var servicePath = match[1].split('/').slice(0, -2).join('/');
                 servicePath = (servicePath) ? '/' + servicePath : '';
 
