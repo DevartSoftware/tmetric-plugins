7a884de38fc3388899eb343fe5a0cf02c99c8ac3
AlexeyN <alexeyn@devart.com>
2015-10-12 18:31:06 +0300

    Initial URL with double slashes, after unsuccessful login it changes them to single and accidentally waitUntil fires.

--- test/webdriverio/customCommands.js
+++ test/webdriverio/customCommands.js
@@ -5,20 +5,25 @@ browser.addCommand("login", function (serviceName) {
   var fullUrl;
   return this.url(service.login.url)
     .url(function (err, res) {
-      fullUrl = res.value.toUpperCase();
+      fullUrl = normalizeUrl(res.value);
     })
     .setValue(service.login.usernameField, service.login.username)
     .setValue(service.login.passwordField, service.login.password)
     .click(service.login.submitButton)
     .waitUntil(function () {
       return browser.url().then(function (res) {
-        return res && res.value && res.value.toUpperCase() != fullUrl
+        return res && res.value && normalizeUrl(res.value) != fullUrl
       });
     }, 5000)
     .url(function (err, res) {
-      if (res.value.toUpperCase() == fullUrl) {
+      if (normalizeUrl(res.value) == fullUrl) {
         // if we are on the same page, this means the login operation failed
         return new Error('Cannot login to ' + serviceName);
       }
     });
-}); 
+});
+
+var normalizeUrl = function (url) {
+  // remove double slashes and trailing slash
+  return url.replace(/\/\//g, '/').replace(/\/$/, '').toUpperCase();
+}
