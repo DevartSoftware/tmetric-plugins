2c83c27ede0d95611fa2bfa3dc1cb4ce727555e0
SergeyMul <sergeymul@devart.com>
2015-10-12 15:27:03 +0300

    rewritten tests
    used common testing commands
    added search for existence of the test issue before checking for existence of the test project

--- test/webdriverio/specs/redmine.spec.js
+++ test/webdriverio/specs/redmine.spec.js
@@ -1,197 +1,121 @@
-fdescribe("Redmine integration spec", function () {
+describe("Redmine integration spec", function () {
 
   var setupError;
-  var redmineUrl = 'http://demo.redmine.org';
-  var testProjectUrl = '';
+  var bugTrackerUrl = 'http://demo.redmine.org';
+
+  var testProjectName = 'redmine-test-qazwsxedc';
+  var testProjectUrl = bugTrackerUrl + '/projects/' + testProjectName;
+
+  var testIssueName = 'Issue for ' + testProjectName;
   var testIssueUrl = '';
-
-  var testProjectName = 'timetracker-redmine-test';
-  var testIssueName = 'Issue for timetracker-redmine-test';
-
-  var testProjectUrl = redmineUrl + '/projects/' + testIssueName;
+  var testIssueSearchUrl = bugTrackerUrl + '/issues?subject=' + testIssueName;
 
   beforeAll(function (done) {
 
-    function checkTestProjectExist () {
+    function getTestIssueUrlFromAnchor () {
       return browser
-        .waitForExist('.projects .project')
-        .elements('.projects .project')
-        .isExisting('a*=' + testProjectName)
+        .getAttribute('a*=' + testIssueName, 'href')
+        .then(function (result) {
+          testIssueUrl = result;
+        })
         ;
     }
 
     function createTestProject () {
       return browser
-        .url(redmineUrl + '/projects/new')
-        .waitForExist('input[name=commit]')
+        .url(bugTrackerUrl + '/projects/new')
         .setValue('#project_name', testProjectName)
         .click('input[name=commit]')
-        .waitUrl(testProjectUrl + '/settings')
+        .waitUrl({url: testProjectUrl + '/settings'})
         .url(testProjectUrl)
-        .waitUrl(testProjectUrl)
-        ;
-    }
-
-    function moveToTestProject () {
-      return browser
-        .elements('.projects .project')
-        .click('a*=' + testProjectName)
-        .waitUrl(testProjectUrl)
-        ;
-    }
-
-    browser
-      .login("TimeTracker")
-      .login("Redmine")
-      .url(redmineUrl + '/projects')
-      .waitForExist('a[href="/projects/new"]')
-      .then(function () {
-        // all logins are successful
-        // now create test project or move to already created
-        return checkTestProjectExist().then(function (result) {
-          if (result) {
-            return moveToTestProject();
-          } else {
-            return createTestProject();
-          }
-        });
-      })
-      .url()
-      .then(function (result) {
-        testProjectUrl = result.value;
-      })
-      .then(done);
-
-  });
-
-  beforeEach(function (done) {
-
-    function checkNoIssuesExist () {
-      return browser
-        .isExisting('p.nodata')
-    }
-
-    function checkTestIssueExist () {
-      return browser
-        .waitForExist('.list.issues .issue')
-        .elements('.list.issues .issue .subject')
-        .isExisting('a*=' + testIssueName)
+        .then(createTestIssue)
         ;
     }
 
     function createTestIssue () {
       return browser
         .url(testProjectUrl + '/issues/new')
-        .waitForExist('input[name=commit]')
         .setValue('#issue_subject', testIssueName)
         .click('input[name=commit]')
-        .waitForExist('.devart-timer-link')
+        .waitForExist('.contextual .icon.icon-del')
+        .url(testProjectUrl + '/issues')
+        .then(getTestIssueUrlFromAnchor)
         ;
     }
 
-    function moveToTestIssue () {
+    function checkTestIssue () {
       return browser
-        .elements('.list.issues .issue .subject')
-        .click('a*=' + testIssueName)
-        .waitForExist('.devart-timer-link')
+        .url(testProjectUrl + '/issues')
+        .isExisting('a*=' + testIssueName)
+        .then(function (result) {
+          return (result ? getTestIssueUrlFromAnchor : createTestIssue)();
+        })
         ;
     }
 
-    function errorHandler (error) {
-      setupError = error;
-      done();
+    function checkTestProject () {
+      return browser
+        .url(bugTrackerUrl + '/projects')
+        .isExisting('a*=' + testProjectName)
+        .then(function (result) {
+          return (result ? checkTestIssue : createTestProject)();
+        })
+        ;
+    }
+
+    function searchTestIssue () {
+      return browser
+        .url(testIssueSearchUrl)
+        .isExisting('a*=' + testIssueName)
+        .then(function (result) {
+          return (result ? getTestIssueUrlFromAnchor : checkTestProject)();
+        })
+        ;      
     }
 
     browser
-      .url(testProjectUrl)
-      .waitForExist('a.issues')
-      .click('a.issues')
-      .waitForExist('p.pagination')
-      .then(function () {
-        return checkNoIssuesExist().then(function (result) {
-          if (result) {
-            return createTestIssue();
-          } else {
-            return checkTestIssueExist().then(function (result) {
-              if (result) {
-                return moveToTestIssue();
-              } else {
-                return createTestIssue();
-              }
-            })
-          }
-        });
-      })
-      .url()
-      .then(function (result) {
-        testIssueUrl = result.value;
-      })
-      .then(done);
+      .login("TimeTracker")
+      .login("Redmine")
+      .then(searchTestIssue)
+      .then(done)
+      ;
 
   });
 
   it("can start tracking time on a task from Redmine test project", function (done) {
 
-    var taskName, projectName, href;
+    var projectName, taskName, taskUrl;
 
     browser
+      .url(testIssueUrl)
       .waitForExist('.devart-timer-link.devart-timer-link-start')
-      .getText('.subject h3').then(function (text) {
-        taskName = text;
-      })
       .getText('#header h1').then(function (text) {
         projectName = text;
       })
-      .url()
-      .then(function (result) {
-        href = result.value;
+      .getText('.subject h3').then(function (text) {
+        taskName = text;
+      })
+      .url().then(function (result) {
+        taskUrl = result.value;
       })
       .click('.devart-timer-link')
       .url('/')
-      .waitForExist('.portlet-body .timer-table')
-      .isVisible('#btn-stop')
-      .then(function (result) {
-        expect(result).toBeTruthy();
-      })
-      .getText('//tr[td/div/span[contains(.,"Active")]]/td[contains(@class,"timer-td-project")]/span').then(function (text) {
-        expect(text).toBe(projectName);
-      })
-      .getText('//tr[td/div/span[contains(.,"Active")]]/td[contains(@class,"timer-td-task")]/div/div/span').then(function (text) {
-        expect(text).toBe(taskName);
-      })
-      .getAttribute('//tr[td/div/span[contains(.,"Active")]]/td[contains(@class,"timer-td-task")]/div/a', 'href').then(function (text) {
-        expect(text).toBe(href);
+      .then(function () {
+        return browser
+          .testActiveTask(projectName, taskName, taskUrl)
+          ;
       })
       .then(done)
       ;
 
   });
 
-  it("can stop tracking time on a task from Redmine test project", function () {
-
-    return browser
-      .waitForExist('.devart-timer-link')
-      .isExisting('.devart-timer-link-start')
-      .then(function (result) {
-        if (result) {
-          return browser
-            .click('.devart-timer-link-start')
-            .waitForExist('.devart-timer-link-stop');
-        }
-      })
-      .click('.devart-timer-link-stop')
-      .url('/')
-      .waitForExist('.portlet-body > div')
-      .isVisible('#btn-stop')
-      .then(function (result) {
-        expect(result).toBeFalsy();
-      })
-      .isExisting('//tr[td/div/span[contains(.,"Active")]]')
-      .then(function (result) {
-        expect(result).toBeFalsy();
-      })
+  it("can stop tracking time on a task from Redmine test project", function (done) {
+    browser
+      .url(testIssueUrl)
+      .stopAndTestTaskAbsent()
+      .then(done)
       ;
-
   });
 
 });
