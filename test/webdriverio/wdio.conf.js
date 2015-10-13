14e727e8c56643a6cbc68ab909a5a51d44a8c916
SergeyMul <sergeymul@devart.com>
2015-10-13 19:34:14 +0300

    configurate mocha

--- test/webdriverio/wdio.conf.js
+++ test/webdriverio/wdio.conf.js
@@ -107,27 +107,10 @@ exports.config = {
     },
 
     //
-    // Options to be passed to Jasmine.
-    jasmineNodeOpts: {
-        //
-        // Jasmine default timeout
-        defaultTimeoutInterval: 60000,
-        //
-        // The Jasmine framework allows it to intercept each assertion in order to log the state of the application
-        // or website depending on the result. For example it is pretty handy to take a screenshot everytime
-        // an assertion fails.
-        expectationResultHandler: function (passed, assertion) {
-
-            /**
-             * only take screenshot if assertion failed
-             */
-            if (passed) {
-                return;
-            }
-
-            var title = assertion.message.replace(/\s/g, '-');
-            browser.saveScreenshot(exports.config.screenshotPath +'assertionError_' + title + '.png');
-        }
+    // Options to be passed to Mocha.
+    // See the full list at http://mochajs.org/
+    mochaOpts: {
+        timeout: 60000
     },
 
     //
@@ -154,7 +137,15 @@ exports.config = {
     // variables like `browser`. It is the perfect place to define custom commands.
     before: function () {
         // do something
-        require('./customCommands')
+        require('./customCommands');
+        
+        var chai = require('chai');
+        var chaiAsPromised = require('chai-as-promised');
+    
+        chai.use(chaiAsPromised);
+        expect = chai.expect;
+        chai.Should();
+        
     },
     //
     // Gets executed after all tests are done. You still have access to all global variables from
