1940ab566ad4eb3e93ff9337d23693be80ae110a
SergeyMul <sergeymul@devart.com>
2015-10-19 15:57:21 +0300

    remove console output

--- test/webdriverio/specs/globalSetup.js
+++ test/webdriverio/specs/globalSetup.js
@@ -1,12 +1,10 @@
 
 before(function () {
-  console.log('before');
   return browser
     .login("TimeTracker")
     .waitForVisible('.page-actions');
 });
 
 beforeEach(function () {
-  console.log('beforeEach');
   return browser.stopRunningTask();
 });
