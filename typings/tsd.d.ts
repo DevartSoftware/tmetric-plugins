5fbee22849d247b8fc255146fd7fef2e73adeab7
SergeyMul <sergeymul@devart.com>
2015-10-23 13:39:08 +0300

    remove jasmine definitions

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -4,6 +4,5 @@
 /// <reference path="webrtc/MediaStream.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="signalr/signalr.d.ts" />
-/// <reference path="jasmine/jasmine.d.ts" />
 /// <reference path="q/Q.d.ts" />
 /// <reference path="webdriverio/webdriverio.d.ts" />
