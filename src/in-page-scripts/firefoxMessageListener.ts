e562049dd27050f75ffb4cab330ca433f2553a19
ArtemA <artema@devart.com>
2016-06-21 15:11:46 +0300

    ver 1.2.0

--- src/in-page-scripts/firefoxMessageListener.ts
+++ src/in-page-scripts/firefoxMessageListener.ts
@@ -2,7 +2,7 @@
     'message',
     event => {
         if (event.data == "version") {
-            window.postMessage({ version: "1.1.0" }, event.origin);
+            window.postMessage({ version: "1.2.0" }, event.origin);
         }
     },
     false);
