095f26c540f02e3141e11ff5aa6baa0dca3483c1
ArtemA <artema@devart.com>
2016-09-22 19:21:39 +0300

    Increase version

--- src/in-page-scripts/firefoxMessageListener.ts
+++ src/in-page-scripts/firefoxMessageListener.ts
@@ -2,7 +2,7 @@
     'message',
     event => {
         if (event.data == "version") {
-            window.postMessage({ version: "1.2.1" }, event.origin);
+            window.postMessage({ version: "1.2.2" }, event.origin);
         }
     },
     false);
