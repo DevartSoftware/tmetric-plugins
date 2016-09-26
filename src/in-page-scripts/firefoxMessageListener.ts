966ade3e458d3743e2aaa22e25bfd84111aefd66
ArtemA <artema@devart.com>
2016-09-26 16:58:57 +0300

    Hotfix for Trello

--- src/in-page-scripts/firefoxMessageListener.ts
+++ src/in-page-scripts/firefoxMessageListener.ts
@@ -2,7 +2,7 @@
     'message',
     event => {
         if (event.data == "version") {
-            window.postMessage({ version: "1.2.1" }, event.origin);
+            window.postMessage({ version: "1.2.1.1" }, event.origin);
         }
     },
     false);
