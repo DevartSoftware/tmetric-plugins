6491395cc2139bac9962eb70537c8ec978ac66dd
ArtemA <artema@devart.com>
2016-09-26 13:48:11 +0300

    TMET-785 - support TestLink 1.9.14

--- src/in-page-scripts/integrations/testLink.ts
+++ src/in-page-scripts/integrations/testLink.ts
@@ -5,48 +5,50 @@
         matchUrl = '*/lib/execute/*.php*';
 
         render(issueElement: HTMLElement, linkElement: HTMLElement) {
-            var host = $$('.groupBtn');
+            let host = $$('.groupBtn');
             if (host) {
                 host.appendChild(linkElement);
             }
         }
 
-        getIssue(issueElement2: HTMLElement, source: Source): WebToolIssue {
+        getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
 
             let titleNode = $$('.exec_tc_title');
             if (!titleNode) {
                 return;
             }
 
+            let projectId = '';
             let issueId = '';
             let issueName = '';
 
-            let i = 0;
+            for (let i = 0; i < titleNode.childNodes.length; i++) {
 
-            while (i < titleNode.childNodes.length) {
                 let childNode = titleNode.childNodes[i];
-                i++;
-                if (childNode.nodeName == '#text') {
 
-                    // nodeValue = '    Test Case ID tmet-1234:: Version: 1		'
-                    let match = /\s+([^\s]+\-[\d]+)\s*\:\:/.exec(childNode.nodeValue);
+                if (childNode.nodeType == Node.TEXT_NODE) {
+
+                    // '    Test Case ID tmet-1234:: Version: 1		'
+                    // '    Test Case tmet-1234 :: Version : 1 :: Case description  '
+                    let match = /\s+([^\s]+)\-([\d]+)\s*::(.*)/.exec(childNode.nodeValue);
                     if (match) {
-                        issueId = match[1]; // tmet-123
+                        projectId = match[1]; // tmet
+                        issueId = `${projectId}-${match[2]}`; // tmet-1234
+                        match = /::(.*)/.exec(match[3]);
+                        if (match) {
+                            issueName = match[1].trim(); // description in same node (ver 1.9.3)
+                        }
+                        if (!issueName) { // description in a separate text node (ver 1.9.14)
+                            for (i++; i < titleNode.childNodes.length; i++) {
+                                childNode = titleNode.childNodes[i];
+                                if (childNode.nodeType == Node.TEXT_NODE) {
+                                    issueName = childNode.nodeValue.trim();
+                                    break;
+                                }
+                            }
+                        }
                         break;
                     }
-                }
-            }
-
-            if (!issueId) {
-                return;
-            }
-
-            while (i < titleNode.childNodes.length) {
-                let childNode = titleNode.childNodes[i];
-                i++;
-                if (childNode.nodeName == '#text') {
-                    issueName = childNode.nodeValue.trim();
-                    break;
                 }
             }
 
@@ -57,7 +59,7 @@
             // http(s)://testlink.company.local/lib/execute/...
             let serviceUrl = source.fullUrl.split('/lib/execute/')[0];
 
-            let issueUrl = `/lib/testcases/archiveData.php?targetTestCase=${issueId}&edit=testcase`;
+            let issueUrl = `/linkto.php?tprojectPrefix=${projectId}&item=testcase&id=${issueId}`;
 
             let projectName = '';
 
