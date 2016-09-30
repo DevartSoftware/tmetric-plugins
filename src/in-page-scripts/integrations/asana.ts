bdd7e041155b7cbc768ce7970afe05e3d734c933
SergeyMul <sergeymul@devart.com>
2016-09-30 13:14:10 +0300

    TMET-806 - added selectors for side and card view of task in asana

--- src/in-page-scripts/integrations/asana.ts
+++ src/in-page-scripts/integrations/asana.ts
@@ -9,7 +9,8 @@
         ];
 
         render(issueElement: HTMLElement, linkElement: HTMLElement) {
-            var host = $$('.sticky-view-placeholder', issueElement);
+            var host = $$('.SingleTaskTitleRow', issueElement) || // side view
+                $$('.sticky-view-placeholder', issueElement); // card view
             if (host) {
                 var linkContainer = $$.create('div', 'devart-timer-link-asana');
                 linkContainer.appendChild(linkElement);
@@ -28,7 +29,7 @@
             var match = /^\/(\w+)(\/search)?\/(\d+)\/(\d+)(\/f)?$/.exec(source.path);
 
             if (match) {
-                var issueId = '#' + match[4]
+                var issueId = '#' + match[4];
 
                 var issueUrl = '/0/0/' + match[4];
 
@@ -37,14 +38,17 @@
                 var serviceUrl = source.protocol + source.host;
             }
 
-            var issueName = $$.try<HTMLTextAreaElement>('#details_property_sheet_title', issueElement).value;
+            var issueName = $$.try<HTMLTextAreaElement>('.SingleTaskTitleRow .simpleTextarea', issueElement).value || //side view
+                $$.try<HTMLTextAreaElement>('#details_property_sheet_title', issueElement).value; // card view
             if (!issueName) {
                 return;
             }
 
             var projectName =
-                $$.try('.tokens-container .token_name').textContent || // task
-                $$.try('.ancestor-projects .token').textContent; // subtask
+                $$.try('.TaskProjectToken-projectName').textContent || //side view task
+                $$.try('.TaskAncestry-ancestorProject').textContent || // side view subtask
+                $$.try('.tokens-container .token_name').textContent || // card view task
+                $$.try('.ancestor-projects .token').textContent; // card view subtask
 
             return { issueId, issueName, projectName, serviceType, serviceUrl, issueUrl };
         }
