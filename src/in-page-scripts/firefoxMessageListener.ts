bf614131e8f8db2a4b5c294a1e8170b65135d98b
ArtemA <artema@devart.com>
2016-11-29 16:38:44 +0200

    v 1.3.1

--- src/in-page-scripts/firefoxMessageListener.ts
+++ src/in-page-scripts/firefoxMessageListener.ts
@@ -2,7 +2,7 @@
     'message',
     event => {
         if (event.data == "version") {
-            window.postMessage({ version: "1.3.0" }, event.origin);
+            window.postMessage({ version: "1.3.1" }, event.origin);
         }
     },
     false);
