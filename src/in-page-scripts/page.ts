d75a1845ad7d84437f30ab73b77f4742ad9ac91e
AlexeyZ <alexeyz@devart.com>
2017-08-02 14:09:45 +0300

    TE-15 - insert phantom version HTML element

--- src/in-page-scripts/page.ts
+++ src/in-page-scripts/page.ts
@@ -136,7 +136,7 @@
     }
 
     /**
-     * Parses issues and adds timer links on a page.
+     * Parses issues, adds timer links on a page, insert phantom version HTML element.
      */
     function parsePage() {
 
@@ -159,6 +159,15 @@
         oldTitle = title;
 
         var { issues, observeMutations } = Integrations.IntegrationService.updateLinks(checkAllIntegrations);
+
+        // Insert phantom HTML element.
+        // This way main app understands that extension is installed.
+        if (/beta\.tmetric\.com/.test(document.URL) || /app\.tmetric\.com/.test(document.URL)) {
+            let span = document.createElement('span');
+            span.style.position = 'absolute';
+            span.id = 'tmetric-extension-installed';
+            document.querySelector('body').appendChild(span);
+        }
 
         if (mutationObserver) {
             // clear queue to prevent observer reentering
