a71cdaf392852e1b7b3e9653baf68f2051fede85
ArtemA <artema@devart.com>
2017-08-10 18:51:09 +0300

    Remove firefoxMessageListener

--- src/in-page-scripts/firefoxMessageListener.ts
+++
@@ -1,8 +0,0 @@
-﻿window.addEventListener(
-    'message',
-    event => {
-        if (event.data == "version") {
-            window.postMessage({ version: "2.1.2" }, event.origin);
-        }
-    },
-    false);
