195e742d7b8b35bde9f1f6c76b89c3e0fc5a6c70
ArtemA <artema@devart.com>
2017-08-10 13:29:11 +0300

    TE-17 - fix project parsing in GitLab

--- src/in-page-scripts/integrations/gitLab.ts
+++ src/in-page-scripts/integrations/gitLab.ts
@@ -14,9 +14,9 @@
         }
 
         render(issueElement: HTMLElement, linkElement: HTMLElement) {
-            var header = $$('.detail-page-header');
+            let header = $$('.detail-page-header');
             if (header) {
-                var buttons = $$('.issue-btn-group', header);
+                let buttons = $$('.issue-btn-group', header);
                 if (buttons) {
                     linkElement.classList.add('btn', 'btn-grouped');
                     buttons.appendChild(linkElement);
@@ -31,42 +31,43 @@
 
             // https://gitlab.com/NAMESPACE/PROJECT/issues/NUMBER
             // https://gitlab.com/NAMESPACE/PROJECT/merge_requests/NUMBER
-            var match = /^(.+)\/(issues|merge_requests)\/(\d+)$/.exec(source.path);
+            let match = /^(.+)\/(issues|merge_requests)\/(\d+)$/.exec(source.path);
 
             if (!match) {
                 return;
             }
 
             // match[3] is a 'NUMBER' from path
-            var issueId = match[3];
+            let issueId = match[3];
             if (!issueId) {
                 return;
             }
 
-            var issueType = match[2];
+            let issueType = match[2];
             issueId = (issueType == 'merge_requests' ? '!' : '#') + issueId;
 
-            var issueNameElement = $$.try('.detail-page-description .title');
-            var issueName = issueNameElement.firstChild ? issueNameElement.firstChild.textContent : issueNameElement.textContent;
+            let issueNameElement = $$.try('.detail-page-description .title');
+            let issueName = issueNameElement.firstChild ? issueNameElement.firstChild.textContent : issueNameElement.textContent;
             if (!issueName) {
                 return;
             }
 
-            var projectNameElement = $$.try('.title .project-item-select-holder').firstChild;
-            var projectName = projectNameElement ?
-                projectNameElement.textContent : // current design
+            let projectNameNode = $$.findNode('.title .project-item-select-holder', Node.TEXT_NODE);
+
+            let projectName = projectNameNode ?
+                projectNameNode.textContent : // current design (both new and old navigation)
                 $$.try('.title > span > a:nth-last-child(2)').textContent; // old design
 
-            var serviceType = 'GitLab';
+            let serviceType = 'GitLab';
 
             // match[1] is a 'https://gitlab.com/NAMESPACE/PROJECT' from path
             // cut '/NAMESPACE/PROJECT' from path
-            var servicePath = match[1].split('/').slice(0, -2).join('/');
+            let servicePath = match[1].split('/').slice(0, -2).join('/');
             servicePath = (servicePath) ? '/' + servicePath : '';
 
-            var serviceUrl = source.protocol + source.host + servicePath;
+            let serviceUrl = source.protocol + source.host + servicePath;
 
-            var issueUrl = $$.getRelativeUrl(serviceUrl, source.path);
+            let issueUrl = $$.getRelativeUrl(serviceUrl, source.path);
 
             return { issueId, issueName, projectName, serviceType, serviceUrl, issueUrl };
         }
