6867b9e54254668f23c9521f3a5a09e260a4176c
Sergey Ivanenko <sergeyi@devart.com>
2017-08-15 13:53:54 +0300

    TE-19 - added integration for Zoho CRM

---
+++ src/in-page-scripts/integrations/zoho.ts
@@ -0,0 +1,53 @@
+﻿module Integrations {
+
+    class Zoho implements WebToolIntegration {
+
+        showIssueId = false;
+
+        observeMutations = true;
+
+        matchUrl = '*/EntityInfo.do*';
+
+        issueElementSelector = '#flw_div table.floatR';
+
+        render(issueElement: HTMLElement, linkElement: HTMLElement) {
+            let container = $$.create('a');
+            container.className = 'newgreybtn newbutton dIB';
+            container.appendChild(linkElement);
+
+            let tdForInsert: Element;
+
+            if (issueElement.querySelectorAll('tr').length == 1) {
+                tdForInsert = issueElement.querySelector('tr td');
+            } else if (issueElement.querySelectorAll('tr').length >= 2) {
+                tdForInsert = issueElement.querySelector('tr:nth-child(2) td');
+            }
+
+            if (tdForInsert.firstElementChild && !tdForInsert.firstElementChild.textContent.length) {
+                tdForInsert.firstElementChild.remove()
+            }
+
+            tdForInsert.insertBefore(container, tdForInsert.firstElementChild);
+        }
+
+        getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
+            let contactName = $$.try('#value_CONTACTID');
+            let issueName = $$.try('#headervalue_SUBJECT, #entityNameInBCView').textContent;
+
+            if (Object.keys(contactName).length && contactName.textContent) {
+                let text = contactName.textContent;
+                issueName = `${$$.try('#headervalue_SUBJECT').textContent} ${text.trim().length ? `- ${text}` : ''}`;
+            }
+
+            let issueId = source.fullUrl.match(/(?:id=(\d+))/)[1];
+            let matches = source.fullUrl.match(/^(.*)(EntityInfo.*)$/);
+
+            let [serviceUrl, issueUrl] = [matches[1], matches[2]]
+            let projectName = null;
+
+            return { issueId, issueName, issueUrl, projectName, serviceUrl, serviceType: 'Zoho' };
+        }
+    }
+
+    IntegrationService.register(new Zoho());
+}
