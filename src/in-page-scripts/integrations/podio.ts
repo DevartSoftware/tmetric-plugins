09e3aa626febeab2062a19337b5e7c1905fc48e7
Sergey Ivanenko <sergeyi@devart.com>
2017-08-17 17:53:40 +0300

    TE-20 - code review

--- src/in-page-scripts/integrations/podio.ts
+++ src/in-page-scripts/integrations/podio.ts
@@ -1,21 +1,25 @@
 ﻿module Integrations {
+
     class Podio implements WebToolIntegration {
+
         showIssueId = false;
 
-        matchUrl = '*://podio.com/tasks*';
+        matchUrl = ['*://podio.com/*'];
 
         render(issueElement: HTMLElement, linkElement: HTMLElement) {
-            var menuList = $$('.task-header .action-bar ul');
+            let menuList = $$('.task-header .action-bar ul');
             if (menuList) {
                 let container = $$.create('li');
-                container.classList.add('float-left');
+                container.classList.add('float-left', 'devart-timer-link-podio');
                 container.appendChild(linkElement);
-                menuList.appendChild(container);
+
+                let togglButton = $$('li > .toggl-button');
+                menuList.insertBefore(container, togglButton && togglButton.parentElement);
 
                 return;
             }
 
-            var rightColumn = $$('div[id*=task-detail-] > div.image-block > .task-right-column');
+            let rightColumn = $$('div[id*=task-detail-] > div.image-block > .task-right-column');
             if (rightColumn) {
                 let container = $$.create('div');
                 container.classList.add('task-via');
@@ -24,21 +28,61 @@
 
                 return;
             }
+
+            let breadcrumb = $$('.breadcrumb');
+            if (breadcrumb) {
+                let container = $$.create('div');
+                container.classList.add('item-via');
+                container.appendChild(linkElement);
+                breadcrumb.parentElement.insertBefore(container, breadcrumb.nextSibling);
+                return;
+            }
         }
         getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
 
-            var issueId: string;
-            var matches = source.path.match(/\d+/);
+            let issueId: string;
+            let matches = source.path.match(/(\/tasks\/\d+)/);
             if (matches) {
-                issueId = matches[0];
+                issueId = matches[1];
             }
 
-            var issueName = $$.try('.ticket-name').textContent;
+            let issueName = this.getTaskName();
+
             var serviceUrl = source.protocol + source.host;
-            var issueUrl = source.path;
-            var projectName = $$.try('.project-name').textContent;
+            var issueUrl = source.fullUrl.match(/\/tasks\/\d+/)[0];
+            var projectName = null;
 
             return { issueId, issueName, issueUrl, projectName, serviceUrl, serviceType: 'Podio' };
+        }
+
+        private getTaskName(): string {
+            let taskName: string;
+
+            // trying to get issueName from task list
+            let task = $$('div[id^="task-detail-"]:not([style*="display: none"])');
+            if (task) {
+                let id = task.id.match(/^task-detail-(\d+)$/)[1]
+                let taskNameWrapper = $$(`li[data-task-id="${id}"] .task-link`);
+                if (taskNameWrapper) {
+                    taskName = taskNameWrapper.textContent;
+                }
+            }
+
+            // trying to get issueName from APP title
+            if (!taskName) {
+                taskName = $$.try('.breadcrumb .item-title').textContent;
+            }
+
+            // trying to get issueName from title of task details page
+            if (!taskName) {
+                taskName = $$.try('div.task-title').textContent;
+            }
+
+            if (taskName) {
+                taskName = taskName.trim();
+            }
+
+            return taskName;
         }
     }
 
