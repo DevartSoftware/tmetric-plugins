b6bac2ad765a224bcf3ecf2ea3ef94db77aee5fe
Sergey Ivanenko <sergeyi@devart.com>
2017-09-21 15:26:08 +0300

    TE-96 - added Gmail

---
+++ src/in-page-scripts/integrations/gsuite.ts
@@ -0,0 +1,43 @@
+﻿module Integrations {
+
+    class GoogleMail implements WebToolIntegration {
+
+        showIssueId = false;
+
+        matchUrl = '*://mail.google.com/mail/*';
+
+        observeMutations = true;
+
+        render(issueElement: HTMLElement, linkElement: HTMLElement) {
+
+            let taskHeader = $$('.ha');
+            if (taskHeader) {
+                linkElement.classList.add('devart-timer-link-gmail');
+                taskHeader.appendChild(linkElement);
+            }
+        }
+
+        getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
+
+            let issueName = $$.try('.ha h2').textContent;
+            if (!issueName) {
+                return;
+            }
+
+            let issueUrl: string;
+            let issueId: string;
+
+            let matches = source.fullUrl.match(/\/mail\/.*#inbox\/([a-zA-Z0-9]+)/);
+            if (matches) {
+                issueUrl = matches[0];
+                issueId = matches[1];
+            }
+
+            var serviceUrl = source.protocol + source.host;
+
+            return { issueId, issueName, issueUrl, serviceUrl, serviceType: 'GoogleMail' };
+        }
+    }
+
+    IntegrationService.register(new GoogleMail());
+}
