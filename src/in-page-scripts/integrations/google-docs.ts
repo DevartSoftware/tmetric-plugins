282c29b7394a980ba8b1297b956e13490f27d49c
Sergey Ivanenko <sergeyi@devart.com>
2017-09-28 18:26:08 +0300

    TE-93 - added Google Docs

---
+++ src/in-page-scripts/integrations/google-docs.ts
@@ -0,0 +1,43 @@
+﻿module Integrations {
+
+    class GoogleDocs implements WebToolIntegration {
+
+        showIssueId = false;
+
+        matchUrl = '*://docs.google.com/document/*';
+
+        observeMutations = true;
+
+        render(issueElement: HTMLElement, linkElement: HTMLElement) {
+
+            let host = $$('#docs-menubar');
+            if (host) {
+                host.appendChild(linkElement);
+            }
+        }
+
+        getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
+
+            let issueName = (<any>$$.try('#docs-titlebar .docs-title-input')).value;
+            if (!issueName) {
+                return;
+            }
+
+            let issueUrl: string;
+            let issueId: string;
+
+            let matches = source.fullUrl.match(/\/document\/d\/([a-zA-Z0-9\-]+)\/edit/);
+
+            if (matches) {
+                issueUrl = matches[0];
+                issueId = matches[1];
+            }
+
+            var serviceUrl = source.protocol + source.host;
+
+            return { issueId, issueName, issueUrl, serviceUrl, serviceType: 'GoogleDocs' };
+        }
+    }
+
+    IntegrationService.register(new GoogleDocs());
+}
