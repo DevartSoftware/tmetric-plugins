e9b2ad5e62cbd82848a4ea2ee2b11d42193cbd28
Sergey Ivanenko <sergeyi@devart.com>
2017-09-28 16:23:57 +0300

    TE-92 - added Calendar + refactoring

---
+++ src/in-page-scripts/integrations/google-calendar.ts
@@ -0,0 +1,42 @@
+﻿module Integrations {
+
+    class GoogleCalendar implements WebToolIntegration {
+
+        showIssueId = false;
+
+        matchUrl = '*://calendar.google.com/calendar/*';
+
+        observeMutations = true;
+
+        render(issueElement: HTMLElement, linkElement: HTMLElement) {
+
+            // detail view
+            let detailedView = $$('.ep .ep-dpc', issueElement);
+            if (detailedView) {
+                linkElement.classList.add('devart-timer-link-calendar-detailed');
+                detailedView.insertBefore(linkElement, detailedView.firstChild);
+            }
+
+            // popup view
+            let popup = $$('.bubblecontent');
+            if (popup) {
+                linkElement.classList.add('devart-timer-link-calendar-popup');
+                popup.insertBefore(linkElement, popup.firstChild);
+            }
+        }
+
+        getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
+
+            let issueName = $$.try('#mtb').textContent || (<any>$$.try('.ep-title input')).value;
+            if (!issueName) {
+                return;
+            }
+
+            var serviceUrl = source.protocol + source.host;
+
+            return { issueName, serviceUrl, serviceType: 'GoogleCalendar' };
+        }
+    }
+
+    IntegrationService.register(new GoogleCalendar());
+}
