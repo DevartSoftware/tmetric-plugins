fa015a0ba0c3a8849cf62bb00c747a49f8547626
AlexeyZ <alexeyz@devart.com>
2017-10-05 11:55:33 +0300

    come back  commits TE-110, TE-111

--- src/in-page-scripts/integrations/todoist.ts
+++ src/in-page-scripts/integrations/todoist.ts
@@ -67,7 +67,9 @@
 
             // Project tab: <a href="#" class="project_link"><span>Личные </span></a>
             // Other tabs: <span class="pname" > Project Name </span>
-            let projectName = $$.try('.pname', issueElement).textContent || $$.try('.project_link').textContent;
+            let projectName = $$.try('.pname', issueElement).textContent
+                || $$.try('.project_link').textContent
+                || $$.try('.project_item__name', issueElement).textContent;
 
             let serviceType = 'Todoist';
             let serviceUrl = source.protocol + source.host;
