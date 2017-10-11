0a2a1316333df101f7d357f4bc6a15edc2f361ed
AlexeyZ <alexeyz@devart.com>
2017-10-11 17:36:58 +0300

    TE-131 - fix flickering popup

--- src/popup/popupActivator.ts
+++ src/popup/popupActivator.ts
@@ -1,6 +1,7 @@
 ﻿if (typeof document != undefined) {
 
     document.body.style.visibility = 'visible'; // Prevent flickering (TE-128)
+    document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'; // Prevent flickering (TE-131)
 
     if (location.search == '?integration') {
         new PagePopupController();
