e09f10bbbc6a60c23e4ca140b0358d8f957c01a2
AlexeyZ <alexeyz@devart.com>
2017-10-12 10:41:46 +0300

    TE-131 - refactoring code

--- src/popup/pagePopupController.ts
+++ src/popup/pagePopupController.ts
@@ -33,7 +33,7 @@ body {
     height: 100%;
     width: 100%;
     padding: 0 !important;
-    background-color: rgba(0, 0, 0, .5);
+    background-color: rgba(0, 0, 0, .5) !important;
 }
 .container {
     position: absolute;
--- src/popup/popupActivator.ts
+++ src/popup/popupActivator.ts
@@ -1,7 +1,6 @@
 ﻿if (typeof document != undefined) {
 
     document.body.style.visibility = 'visible'; // Prevent flickering (TE-128)
-    document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'; // Prevent flickering (TE-131)
 
     if (location.search == '?integration') {
         new PagePopupController();
