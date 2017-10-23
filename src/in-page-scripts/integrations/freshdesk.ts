6606607b020436bb081e956375a758b5ec4c4bc1
Sergey Ivanenko <sergeyi@devart.com>
2017-10-23 16:21:23 +0300

    TE-151 - fixed TMetric button for new freshdesk

--- src/in-page-scripts/integrations/freshdesk.ts
+++ src/in-page-scripts/integrations/freshdesk.ts
@@ -1,10 +1,10 @@
-﻿class Freshdesk implements WebToolIntegration {
+﻿class OldFreshdesk implements WebToolIntegration {
 
     showIssueId = true;
 
     observeMutations = true;
 
-    matchUrl = '*.freshdesk.com/helpdesk/tickets/*';
+    matchUrl = '*://*/helpdesk/tickets/*';
 
     render(issueElement: HTMLElement, linkElement: HTMLElement) {
 
@@ -34,8 +34,79 @@
             projectName = "";
         }
 
-        return { issueId, issueName, issueUrl, projectName, serviceUrl, serviceType: 'Freshdesk' };
+        return { issueId, issueName, issueUrl, projectName, serviceUrl, serviceType: 'OldFreshdesk' };
     }
 }
 
-IntegrationService.register(new Freshdesk());
+class NewFreshdesk implements WebToolIntegration {
+
+    showIssueId = true;
+
+    observeMutations = true;
+
+    matchUrl = '*://*/a/tickets/*';
+
+    render(issueElement: HTMLElement, linkElement: HTMLElement) {
+
+        var host = $$('.page-actions__left');
+        if (host) {
+            let container = $$.create('button',
+                'app-icon-btn', 'app-icon-btn--text', 'devart-timer-link-freshdesk'
+            );
+            container.appendChild(linkElement);
+            host.appendChild(container);
+        }
+    }
+
+    getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
+
+        //https://company.freshdesk.com/a/tickets/1
+        var issueName = $$.try('.description-subject').textContent;
+
+        let issueId: string;
+        let issueUrl: string;
+
+        let matches = source.path.match(/\/a\/tickets\/(\d+)/);
+        if (matches) {
+            issueUrl = matches[0];
+            issueId = matches[1];
+        }
+
+        var serviceUrl = source.protocol + source.host;
+
+        let projectName: string;
+
+        let projectNameContainer: HTMLElement;
+
+        $$.all('.label-field').forEach(label => {
+            if (projectNameContainer = this.getProjectContainer(label)) {
+                return;
+            }
+        })
+
+        if (projectNameContainer) {
+            projectName = $$.try('.ember-power-select-selected-item', projectNameContainer).textContent;
+            if (projectName) {
+                projectName = projectName.trim();
+            }
+        }
+
+        if (projectName === '--') {
+            projectName = "";
+        }
+
+        return { issueId, issueName, issueUrl, projectName, serviceUrl, serviceType: 'NewFreshdesk' };
+    }
+
+    private getProjectContainer(label: HTMLElement) {
+        // hardcoded label names
+        switch (label.textContent) {
+            case 'Product':
+                return label.parentElement;
+            default:
+                return null;
+        }
+    }
+}
+
+IntegrationService.register(new OldFreshdesk(), new NewFreshdesk());
