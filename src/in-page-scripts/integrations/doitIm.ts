cf531b217f1e9b11a354896e94f52ad5ecaeae55
Sergey Ivanenko <sergeyi@devart.com>
2017-11-22 15:32:02 +0200

    TE-200 - added integration with doit.im

---
+++ src/in-page-scripts/integrations/doitIm.ts
@@ -0,0 +1,124 @@
+﻿class DoitImTask implements WebToolIntegration {
+
+    showIssueId = false;
+
+    matchUrl = 'https://i.doit.im/home/#/task/*';
+
+    observeMutations = true;
+
+    render(issueElement: HTMLElement, linkElement: HTMLElement) {
+
+        let toolbar = $$('li.task-op', issueElement);
+        if (toolbar) {
+            linkElement.classList.add('btn-4');
+            toolbar.insertBefore(linkElement, toolbar.firstChild);
+        }
+
+    }
+
+    getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
+
+        let issueName = $$.try('#task_container span.title').textContent;
+        if (!issueName) {
+            return;
+        }
+
+        let issueUrl: string;
+        let issueId: string;
+
+        let matches = source.fullUrl.match(/\/home\/#\/task\/([a-z0-9\-])+/);
+
+        if (matches) {
+            issueUrl = matches[0];
+            issueId = matches[1];
+        }
+
+        var serviceUrl = source.protocol + source.host;
+        
+        return { issueId, issueName, issueUrl, serviceUrl, serviceType: 'DoitIm' };
+    }
+}
+
+class DoitImProject implements WebToolIntegration {
+
+    showIssueId = false;
+
+    matchUrl = 'https://i.doit.im/home/#/project/*';
+
+    observeMutations = true;
+
+    render(issueElement: HTMLElement, linkElement: HTMLElement) {
+
+        let toolbar = $$('li.project-op', issueElement);
+        if (toolbar) {
+            linkElement.classList.add('btn-4');
+            toolbar.insertBefore(linkElement, toolbar.firstChild);
+        }
+
+    }
+
+    getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
+
+        let issueName = $$.try('#project_info span.title').textContent;
+        if (!issueName) {
+            return;
+        }
+
+        let issueUrl: string;
+        let issueId: string;
+
+        let matches = source.fullUrl.match(/\/home\/#\/project\/([a-z0-9\-])+/);
+
+        if (matches) {
+            issueUrl = matches[0];
+            issueId = matches[1];
+        }
+
+        var serviceUrl = source.protocol + source.host;
+
+        return { issueId, issueName, issueUrl, serviceUrl, serviceType: 'DoitIm' };
+    }
+}
+
+class DoitImGoal implements WebToolIntegration {
+
+    showIssueId = false;
+
+    matchUrl = 'https://i.doit.im/home/#/goal/*';
+
+    observeMutations = true;
+
+    render(issueElement: HTMLElement, linkElement: HTMLElement) {
+
+        let toolbar = $$('li.goal-op', issueElement);
+        if (toolbar) {
+            linkElement.classList.add('btn-4');
+            toolbar.insertBefore(linkElement, toolbar.firstChild);
+        }
+
+    }
+
+    getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
+
+        let issueName = $$.try('#goal_info span.title').textContent;
+        if (!issueName) {
+            return;
+        }
+
+        let issueUrl: string;
+        let issueId: string;
+
+        let matches = source.fullUrl.match(/\/home\/#\/goal\/([a-z0-9\-])+/);
+
+        if (matches) {
+            issueUrl = matches[0];
+            issueId = matches[1];
+        }
+
+        var serviceUrl = source.protocol + source.host;
+
+        return { issueId, issueName, issueUrl, serviceUrl, serviceType: 'DoitIm' };
+    }
+}
+
+IntegrationService.register(new DoitImTask(), new DoitImProject(), new DoitImGoal());
