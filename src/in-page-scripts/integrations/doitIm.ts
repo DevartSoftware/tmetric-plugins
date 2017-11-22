1e62c1ce9e9096b3491e9878ade603e566ceb6d2
ArtemA <artema@devart.com>
2017-11-22 16:24:23 +0200

    TE-200 - refactoring

--- src/in-page-scripts/integrations/doitIm.ts
+++ src/in-page-scripts/integrations/doitIm.ts
@@ -1,14 +1,22 @@
-﻿class DoitImTask implements WebToolIntegration {
+﻿class DoitIm implements WebToolIntegration {
 
     showIssueId = false;
 
-    matchUrl = 'https://i.doit.im/home/#/task/*';
+    matchUrl: RegExp;
+
+    constructor(private _issueType: string, private _titleSelector: string) {
+
+        // https://i.doit.im/home/#/task/99999999-aaaa-bbbb-cccc-dddddddddddd
+        // https://i.doit.im/home/#/project/99999999-aaaa-bbbb-cccc-dddddddddddd
+        // https://i.doit.im/home/#/goal/99999999-aaaa-bbbb-cccc-dddddddddddd
+        this.matchUrl = new RegExp(`(https://i.doit.im)(/home/#/${_issueType}/([a-z0-9\-]+))`);
+    }
 
     observeMutations = true;
 
     render(issueElement: HTMLElement, linkElement: HTMLElement) {
 
-        let toolbar = $$('li.task-op', issueElement);
+        let toolbar = $$(`li.${this._issueType}-op`, issueElement);
         if (toolbar) {
             linkElement.classList.add('btn-4');
             toolbar.insertBefore(linkElement, toolbar.firstChild);
@@ -18,107 +26,18 @@
 
     getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
 
-        let issueName = $$.try('#task_container span.title').textContent;
-        if (!issueName) {
-            return;
-        }
+        let issueName = $$.try(`${this._titleSelector} span.title`).textContent;
 
-        let issueUrl: string;
-        let issueId: string;
-
-        let matches = source.fullUrl.match(/\/home\/#\/task\/([a-z0-9\-])+/);
-
-        if (matches) {
-            issueUrl = matches[0];
-            issueId = matches[1];
-        }
-
-        var serviceUrl = source.protocol + source.host;
+        let matches = source.fullUrl.match(this.matchUrl);
+        let serviceUrl = matches[1];
+        let issueUrl = matches[2];
+        let issueId = matches[3];
         
-        return { issueId, issueName, issueUrl, serviceUrl, serviceType: 'DoitIm' };
+        return { issueId, issueName, issueUrl, serviceUrl, serviceType: 'Doit.im' };
     }
 }
 
-class DoitImProject implements WebToolIntegration {
-
-    showIssueId = false;
-
-    matchUrl = 'https://i.doit.im/home/#/project/*';
-
-    observeMutations = true;
-
-    render(issueElement: HTMLElement, linkElement: HTMLElement) {
-
-        let toolbar = $$('li.project-op', issueElement);
-        if (toolbar) {
-            linkElement.classList.add('btn-4');
-            toolbar.insertBefore(linkElement, toolbar.firstChild);
-        }
-
-    }
-
-    getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
-
-        let issueName = $$.try('#project_info span.title').textContent;
-        if (!issueName) {
-            return;
-        }
-
-        let issueUrl: string;
-        let issueId: string;
-
-        let matches = source.fullUrl.match(/\/home\/#\/project\/([a-z0-9\-])+/);
-
-        if (matches) {
-            issueUrl = matches[0];
-            issueId = matches[1];
-        }
-
-        var serviceUrl = source.protocol + source.host;
-
-        return { issueId, issueName, issueUrl, serviceUrl, serviceType: 'DoitIm' };
-    }
-}
-
-class DoitImGoal implements WebToolIntegration {
-
-    showIssueId = false;
-
-    matchUrl = 'https://i.doit.im/home/#/goal/*';
-
-    observeMutations = true;
-
-    render(issueElement: HTMLElement, linkElement: HTMLElement) {
-
-        let toolbar = $$('li.goal-op', issueElement);
-        if (toolbar) {
-            linkElement.classList.add('btn-4');
-            toolbar.insertBefore(linkElement, toolbar.firstChild);
-        }
-
-    }
-
-    getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
-
-        let issueName = $$.try('#goal_info span.title').textContent;
-        if (!issueName) {
-            return;
-        }
-
-        let issueUrl: string;
-        let issueId: string;
-
-        let matches = source.fullUrl.match(/\/home\/#\/goal\/([a-z0-9\-])+/);
-
-        if (matches) {
-            issueUrl = matches[0];
-            issueId = matches[1];
-        }
-
-        var serviceUrl = source.protocol + source.host;
-
-        return { issueId, issueName, issueUrl, serviceUrl, serviceType: 'DoitIm' };
-    }
-}
-
-IntegrationService.register(new DoitImTask(), new DoitImProject(), new DoitImGoal());
+IntegrationService.register(
+    new DoitIm('task', '#task_container'),
+    new DoitIm('project', '#project_info'),
+    new DoitIm('goal', '#goal_info'));
