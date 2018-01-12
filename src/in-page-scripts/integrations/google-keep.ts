3ede551792684d9c169d12b4cf1d099177bcdde4
Sergey Ivanenko <sergeyi@devart.com>
2018-01-12 18:58:14 +0200

    TE-217 - fixed code style for OpenProject + removed module declaration

--- src/in-page-scripts/integrations/google-keep.ts
+++ src/in-page-scripts/integrations/google-keep.ts
@@ -1,57 +1,54 @@
-﻿module Integrations {
+﻿class GoogleKeep implements WebToolIntegration {
 
-    class GoogleKeep implements WebToolIntegration {
+    showIssueId = false;
 
-        showIssueId = false;
+    matchUrl = '*://keep.google.com/*#*';
 
-        matchUrl = '*://keep.google.com/*#*';
+    issueElementSelector = [
+        '.XKSfm-L9AdLc .CmABtb.RNfche', // selector for row with checkbox (in the checklist)
+        '.XKSfm-L9AdLc .IZ65Hb-yePe5c'  // selector for toolbar
+    ];
 
-        issueElementSelector = [
-            '.XKSfm-L9AdLc .CmABtb.RNfche', // selector for row with checkbox (in the checklist)
-            '.XKSfm-L9AdLc .IZ65Hb-yePe5c'  // selector for toolbar
-        ];
+    observeMutations = true;
 
-        observeMutations = true;
+    render(issueElement: HTMLElement, linkElement: HTMLElement) {
+        linkElement.classList.add('devart-timer-link-minimal');
 
-        render(issueElement: HTMLElement, linkElement: HTMLElement) {
-            linkElement.classList.add('devart-timer-link-minimal');
-
-            if (issueElement.matches(this.issueElementSelector[0])) { // for checklist
-                linkElement.classList.add('devart-timer-link-google-keep-item')
-                issueElement.insertBefore(linkElement, issueElement.querySelector('div[role="button"]:last-child'));
-            } else if (issueElement.matches(this.issueElementSelector[1])) { // for note
-                linkElement.classList.add('devart-timer-link-google-keep-note')
-                let toolbar = issueElement.querySelector('[role="toolbar"]');
-                toolbar.appendChild(linkElement);
-            }
-        }
-
-        getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
-
-            let issueName: string;
-            let issueUrl: string;
-            let issueId: string;
-            let serviceUrl: string;
-
-            if (issueElement.matches(this.issueElementSelector[0])) { // if checklist
-                issueName = $$.try('.notranslate', issueElement).textContent;
-            } else if (issueElement.matches(this.issueElementSelector[1])) {
-                let card = $$.closest('.XKSfm-L9AdLc', issueElement);
-                if (card) {
-                    issueName = $$('.notranslate', card).textContent;
-                }
-
-                let matches = source.fullUrl.match(/\/#[a-zA-Z]+\/(\d+\.\d+)/);
-                if (matches) {
-                    issueUrl = matches[0];
-                    issueId = matches[1];
-                    serviceUrl = source.protocol + source.host;
-                }
-            }
-
-            return { issueUrl, issueId, issueName, serviceUrl, serviceType: 'GoogleKeep' };
+        if (issueElement.matches(this.issueElementSelector[0])) { // for checklist
+            linkElement.classList.add('devart-timer-link-google-keep-item')
+            issueElement.insertBefore(linkElement, issueElement.querySelector('div[role="button"]:last-child'));
+        } else if (issueElement.matches(this.issueElementSelector[1])) { // for note
+            linkElement.classList.add('devart-timer-link-google-keep-note')
+            let toolbar = issueElement.querySelector('[role="toolbar"]');
+            toolbar.appendChild(linkElement);
         }
     }
 
-    IntegrationService.register(new GoogleKeep());
+    getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
+
+        let issueName: string;
+        let issueUrl: string;
+        let issueId: string;
+        let serviceUrl: string;
+
+        if (issueElement.matches(this.issueElementSelector[0])) { // if checklist
+            issueName = $$.try('.notranslate', issueElement).textContent;
+        } else if (issueElement.matches(this.issueElementSelector[1])) {
+            let card = $$.closest('.XKSfm-L9AdLc', issueElement);
+            if (card) {
+                issueName = $$('.notranslate', card).textContent;
+            }
+
+            let matches = source.fullUrl.match(/\/#[a-zA-Z]+\/(\d+\.\d+)/);
+            if (matches) {
+                issueUrl = matches[0];
+                issueId = matches[1];
+                serviceUrl = source.protocol + source.host;
+            }
+        }
+
+        return { issueUrl, issueId, issueName, serviceUrl, serviceType: 'GoogleKeep' };
+    }
 }
+
+IntegrationService.register(new GoogleKeep());
--- src/in-page-scripts/integrations/openProject.ts
+++ src/in-page-scripts/integrations/openProject.ts
@@ -1,69 +1,39 @@
-module Integrations {
-  class OpenProject implements WebToolIntegration {
-    /**
-     * The array of URLs (with wildcards) that are used to identify
-     * pages as those that belong to the service.
-     */
-    matchUrl = [
-      '*://*.openproject.com/projects/*/work_packages/*',
-      '*://*.openproject.com/work_packages/*'
-    ];
+class OpenProject implements WebToolIntegration {
 
-    /**
-     * If the service may be on a custom domain implement this method
-     * to identify pages of the service by other features (e.g. meta-tags).
-     */
-    match(source: Source): boolean {
-      return $$.getAttribute('body', 'ng-app') == 'openproject';
-    }
-
-    /**
-     * observeMutations = true means that the extension observes the page for
-     * dynamic data loading. This means that, if the tool loads some parts of
-     * the page with AJAX or generates dynamically, the TMetric extension waits
-     * until all loading is done and then adds the button to the page.
-     */
-    observeMutations = true;
     showIssueId = true;
 
+    matchUrl = [
+        '*://*.openproject.com/projects/*/work_packages/*',
+        '*://*.openproject.com/work_packages/*'
+    ];
 
-    /**
-     * Extracts information about the issue (ticket or task) from a Web
-     * page by querying the DOM model.
-     */
-    getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
-      let issueId = $$.try('.work-packages--info-row span')[0].textContent;
-      let issueName = $$.try('span.subject').textContent;
-      let serviceUrl = source.protocol + source.host;
-      let issueUrl = source.path;
-      let projectName =
-          $$.try('#projects-menu .button--dropdown-text').textContent ||
-          $$.try('.-project-context span a').textContent;
-      let tagNames = $$.all('.labels').map(label => label.textContent);
-
-      return {
-        issueId,
-        issueName,
-        issueUrl,
-        projectName,
-        serviceUrl,
-        serviceType: 'OpenProject',
-        tagNames
-      };
+    match(source: Source): boolean {
+        return $$.getAttribute('body', 'ng-app') == 'openproject';
     }
 
-    /**
-     * Inserts the timer button for the identified issue into a Web page.
-     */
+    observeMutations = true;
+
     render(issueElement: HTMLElement, linkElement: HTMLElement) {
-      let host = $$('#toolbar-items');
-      if (host) {
-        var container = $$.create('li', 'toolbar-item');
-        container.appendChild(linkElement);
-        host.appendChild(container);
-      }
+        let host = $$('#toolbar-items');
+        if (host) {
+            var container = $$.create('li', 'toolbar-item');
+            container.appendChild(linkElement);
+            host.appendChild(container);
+        }
     }
-  }
 
-  IntegrationService.register(new OpenProject());
+    getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
+        let issueId = $$.try('.work-packages--info-row span')[0].textContent;
+        let issueName = $$.try('span.subject').textContent;
+        let serviceUrl = source.protocol + source.host;
+        let issueUrl = source.path;
+        let projectName =
+            $$.try('#projects-menu .button--dropdown-text').textContent ||
+            $$.try('.-project-context span a').textContent;
+        let tagNames = $$.all('.labels').map(label => label.textContent);
+
+        return { issueId, issueName, issueUrl, projectName, serviceUrl, serviceType: 'OpenProject', tagNames };
+    }
 }
+
+IntegrationService.register(new OpenProject());
