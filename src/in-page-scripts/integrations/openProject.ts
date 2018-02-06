1b31ffc1052e22519f62ed70744df125e3d2a133
Sergey Ivanenko <sergeyi@devart.com>
2018-02-06 16:56:50 +0200

    TE-239 - fixed displaying timer button for openproject

--- src/in-page-scripts/integrations/openProject.ts
+++ src/in-page-scripts/integrations/openProject.ts
@@ -1,8 +1,8 @@
-class OpenProject implements WebToolIntegration {
+class OpenProjectFullScreenView implements WebToolIntegration {
 
     showIssueId = true;
 
-    matchUrl = /(https:\/\/.+\.openproject\.com).*\/work_packages\/\D*(\d+)/;
+    matchUrl = /(https:\/\/.+\.openproject\.com).*\/work_packages\/(\d+)/;
 
     match(source: Source): boolean {
         return $$.getAttribute('body', 'ng-app') == 'openproject';
@@ -35,4 +35,44 @@ class OpenProject implements WebToolIntegration {
     }
 }
 
-IntegrationService.register(new OpenProject());
+class OpenProjectDetailedView implements WebToolIntegration {
+
+    showIssueId = true;
+
+    matchUrl = /(https:\/\/.+\.openproject\.com).*\/work_packages\/\D*(\d+)/;
+
+    match(source: Source): boolean {
+        return $$.getAttribute('body', 'ng-app') == 'openproject';
+    }
+
+    observeMutations = true;
+
+    render(issueElement: HTMLElement, linkElement: HTMLElement) {
+        let detailedView = $$('.work-package--single-view');
+        if (detailedView) {
+            let infoWrapper = $$('.wp-info-wrapper');
+            if (infoWrapper) {
+                let container = $$.create('div');
+                container.classList.add('devart-timer-link-openproject-container')
+                container.appendChild(linkElement);
+                detailedView.insertBefore(container, infoWrapper.nextElementSibling)
+            }
+        }
+    }
+
+    getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
+
+        let match = source.fullUrl.match(this.matchUrl)
+        let serviceUrl = match[1];
+        let issueUrl = '/work_packages/' + match[2];
+        let issueId = '#' + match[2];
+        let issueName = $$.try('.work-packages--subject-type-row span.subject').textContent;
+        let projectName =
+            $$.try('#projects-menu').textContent ||
+            $$.try('.-project-context span a').textContent;
+
+        return { issueId, issueName, issueUrl, projectName, serviceUrl, serviceType: 'OpenProject' };
+    }
+}
+
+IntegrationService.register(new OpenProjectFullScreenView(), new OpenProjectDetailedView());
