3416e2596cca0f069b55f394fed7fcc95a2b3438
Artem Avramenko <artema@devart.com>
2018-03-21 18:32:54 +0200

    ver 2.7.1

--- src/in-page-scripts/version.ts
+++ src/in-page-scripts/version.ts
@@ -15,7 +15,7 @@
     }
 
     let extensionInfo = { // object is updated from gulp build
-        version: '2.7.0'
+        version: '2.7.1'
     };
 
     let metaName = 'tmetric-extension-version';
