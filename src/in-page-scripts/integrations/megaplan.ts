5cf3a7a221849670a77c0b2211aa121e6be3be55
Artem Avramenko <artema@devart.com>
2018-04-05 14:25:58 +0300

    TE-269 - fixed formatting

--- src/in-page-scripts/integrations/megaplan.ts
+++ src/in-page-scripts/integrations/megaplan.ts
@@ -1,37 +1,37 @@
-class Megaplan implements WebToolIntegration{
-   
+class Megaplan implements WebToolIntegration {
+
     showIssueId = false;
     observeMutations = true;
     matchUrl = '*.megaplan.ru/task/*/card';
-    
+
     render(issueElement: HTMLElement, linkElement: HTMLElement) {
+
         var host = $$.try('._1XKpckMR-iqskJqYp7ANvs.undefinedChild.LI-DbNJ989DALGWiPbU2N._1RspPkSKS8mI1tTV5bPJOK');
 
-if (host) {
-	console.log(host);
-	linkElement.classList.add('devart-timer-link-todoist');
-        host.parentElement.appendChild(linkElement);
+        if (host) {
+            linkElement.classList.add('devart-timer-link-todoist');
+            host.parentElement.appendChild(linkElement);
+        }
     }
-    }
-    getIssue(issueElement: HTMLElement, source: Source) : WebToolIssue{
-	console.log("Checking");
+    getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
+
         let issueObj = $$.try('._1XKpckMR-iqskJqYp7ANvs.undefinedChild.LI-DbNJ989DALGWiPbU2N._1RspPkSKS8mI1tTV5bPJOK');
-	if (!issueObj){
-		return;
-	}
-	let issueName='';
-	let issueNumber = source.path.split('/')[2]; 
-	if (!issueNumber) {
+        if (!issueObj) {
             return;
         }
-        
-	let issueId = '#' + issueNumber;
-	if ('firstChild' in issueObj){
-	 issueName=issueObj.firstChild.textContent;
-	} else return;
+        let issueName = '';
+        let issueNumber = source.path.split('/')[2];
+        if (!issueNumber) {
+            return;
+        }
+
+        let issueId = '#' + issueNumber;
+        if ('firstChild' in issueObj) {
+            issueName = issueObj.firstChild.textContent;
+        } else return;
 
         let projectName = $$.try('._3bCrSGVnXH5AUbZRlD6TbT').textContent;
-	if (!projectName) {projectName='';}
+        if (!projectName) { projectName = ''; }
         let serviceType = 'Megaplan';
         let serviceUrl = source.protocol + source.host;
         let issueUrl = 'showTask?id=' + issueNumber;
@@ -39,4 +39,5 @@ if (host) {
         return { issueId, issueName, projectName, serviceType, serviceUrl, issueUrl, tagNames };
     }
 }
+
 IntegrationService.register(new Megaplan());
