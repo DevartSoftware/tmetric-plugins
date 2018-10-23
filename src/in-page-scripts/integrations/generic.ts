36134bf71e9bd1ce0c06bbe2564e63eb59a38ab3
Artem Avramenko <artema@devart.com>
2018-10-23 14:43:26 +0300

    Generic integrations

---
+++ src/in-page-scripts/integrations/generic.ts
@@ -0,0 +1,37 @@
+﻿class Generic implements WebToolIntegration {
+
+    showIssueId = true;
+
+    observeMutations = true;
+
+    issueElementSelector = '.tmetric-button';
+
+    match(source: Source): boolean {
+        return !!$$('.tmetric-button');
+    }
+
+    render(issueElement: HTMLElement, linkElement: HTMLElement) {
+        issueElement.appendChild(linkElement);
+    }
+
+    getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
+        let issueId = issueElement.getAttribute('data-issue-id');
+        let issueName = issueElement.getAttribute('data-issue-name');
+        let serviceUrl = issueElement.getAttribute('data-service-url');
+        let issueUrl = issueElement.getAttribute('data-issue-url');
+        let projectName = issueElement.getAttribute('data-project-name');
+        let tagNames = issueElement.getAttribute('data-tag-names').split(',');
+
+        return {
+            issueId,
+            issueName,
+            issueUrl,
+            projectName,
+            serviceUrl,
+            serviceType: 'Generic',
+            tagNames
+        };
+    }
+}
+
+IntegrationService.register(new Generic());
