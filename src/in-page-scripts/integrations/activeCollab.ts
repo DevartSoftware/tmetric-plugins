e24e6ef5ce76f14ef6cd90f211e9337bbff2f028
Sergey Ivanenko <sergeyi@devart.com>
2018-11-05 16:17:48 +0200

    TE-340 - validated user integration code

--- src/in-page-scripts/integrations/activeCollab.ts
+++ src/in-page-scripts/integrations/activeCollab.ts
@@ -1,54 +1,50 @@
 class ActiveCollab implements WebToolIntegration {
-  /**
-   * The array of URLs (with wildcards) that are used to identify
-   * pages as those that belong to the service.
-   */
-  matchUrl = '*://*/projects/*';
 
-  showIssueId = true;
+    matchUrl = '*://*/projects/*';
 
-  observeMutations = true;
+    showIssueId = true;
 
-  issueElementSelector = [
-    '.object_view_main',          // task
-  ]
+    observeMutations = true;
 
-  render(issueElement: HTMLElement, linkElement: HTMLElement) {
+    issueElementSelector = [
+        '.object_view_main',          // task
+    ]
 
-    let existing = $$('div.object_view_sidebar .tmetric');
+    render(issueElement: HTMLElement, linkElement: HTMLElement) {
 
-    let host = $$('div.object_view_sidebar');
-    if (host) {
-      if (existing) {
-        host.removeChild(existing);
-      }
+        let existing = $$('div.object_view_sidebar .tmetric');
 
-      let newdiv = document.createElement("div");
-      newdiv.classList.add('page_section');
-      newdiv.classList.add('with_padding');
-      newdiv.classList.add('tmetric');
-      newdiv.appendChild(linkElement);
-      host.appendChild(newdiv);
-    }
-  }
+        let host = $$('div.object_view_sidebar');
+        if (host) {
+            if (existing) {
+                host.removeChild(existing);
+            }
 
-  getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
+            let newdiv = document.createElement("div");
+            newdiv.classList.add('page_section', 'with_padding', 'tmetric');
+            newdiv.appendChild(linkElement);
 
-    let issueName = (<any>$$.try('#project_task .task_name')).textContent;
-    if (!issueName) {
-      return;
+            host.appendChild(newdiv);
+        }
     }
 
-    let projectName = (<any>$$.try('#project_task a[data-qa-id="task-project-label-name"]')).textContent;
-    let issueId = (<any>$$.try('#project_task span[ng-bind="task.task_number"]')).textContent;
-    if (issueId && projectName) {
-      let serviceUrl = source.protocol + source.host;
-      let serviceType = 'ActiveCollab';
-      let issueUrl = $$.getRelativeUrl(serviceUrl, source.fullUrl);
+    getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
 
-      return { issueId, issueName, projectName, serviceType, serviceUrl, issueUrl };
+        let issueName = (<any>$$.try('#project_task .task_name')).textContent;
+        if (!issueName) {
+            return;
+        }
+
+        let projectName = (<any>$$.try('#project_task a[data-qa-id="task-project-label-name"]')).textContent;
+        let issueId = (<any>$$.try('#project_task span[ng-bind="task.task_number"]')).textContent;
+        if (issueId && projectName) {
+            let serviceUrl = source.protocol + source.host;
+            let serviceType = 'ActiveCollab';
+            let issueUrl = $$.getRelativeUrl(serviceUrl, source.fullUrl);
+
+            return { issueId, issueName, projectName, serviceType, serviceUrl, issueUrl };
+        }
     }
-  }
 }
 
 IntegrationService.register(new ActiveCollab());
