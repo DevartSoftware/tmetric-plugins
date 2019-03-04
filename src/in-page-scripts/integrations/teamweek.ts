131f5032d382b15275a7902c1bff0391b15c4f53
SergeyMul <sergeymul@devart.com>
2019-03-04 21:24:33 +0200

    TE-385 - updated Teamweek integration

--- src/in-page-scripts/integrations/teamweek.ts
+++ src/in-page-scripts/integrations/teamweek.ts
@@ -8,34 +8,30 @@
     // https://app.teamweek.com/#timeline/task/WORKSPACE_ID/TASK_ID
     matchUrl = '*://app.teamweek.com/#timeline*';
 
-    issueElementSelector = '.timeline-task-popup';
+    issueElementSelector = '.timeline-task-modal';
 
     render(issueElement: HTMLElement, linkElement: HTMLElement) {
         var host = $$('[data-hook="row-actions"]', issueElement);
         if (host) {
-            linkElement.classList.add(
-                'devart-timer-link-teamweek',
-                'devart-timer-link-minimal',
-                'button', 'button--large', 'button--icon');
+            linkElement.classList.add('row', 'row--align-center', 'font-tiny');
             host.appendChild(linkElement);
         }
     }
 
     getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
 
-        var issueName = $$.try<HTMLInputElement>('[data-hook="input-name"]', issueElement).value;
+        var issueName = $$.try<HTMLInputElement>('input[name=name]', issueElement).value;
         if (!issueName) {
             return;
         }
 
-        var match = /^(.+)\/(#timeline\/task\/\d+)\/(\d+)$/.exec(source.fullUrl);
-
-        var projectName = $$.try<HTMLInputElement>('[data-hook="input-project"]', issueElement).value;
-
+        var match = /^(.+)\/(#timeline\/\d+\/tasks)\/(\d+)$/.exec(source.fullUrl);
         if (match) {
             var issueId = '#' + match[3];
             var issueUrl = match[2] + '/' + match[3];
         }
+
+        var projectName = $$.try<HTMLInputElement>('input[name=project_name]', issueElement).value;
 
         var serviceType = 'Teamweek';
         var serviceUrl = source.protocol + source.host;
