adda487729ba8338bd69f175a024647f0f48d198
Artem Avramenko <artema@devart.com>
2019-08-09 17:36:39 +0300

    ver 2.12.0

--- setVersion.bat
+++ setVersion.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d %~dp0
 
-call npx --no-install gulp version --newversion=2.11.0
+call npx --no-install gulp version --newversion=2.12.0
--- src/in-page-scripts/version.ts
+++ src/in-page-scripts/version.ts
@@ -15,7 +15,7 @@
     }
 
     let extensionInfo = { // object is updated from gulp build
-        version: '2.11.0'
+        version: '2.12.0'
     };
 
     let metaName = 'tmetric-extension-version';
