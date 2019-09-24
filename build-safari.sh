4efc98f0d1051db097098a49cae2d092a4f035f5
SergeyMul <sergeymul@devart.com>
2019-09-24 20:03:06 +0300

    TE-441 - added service url replacing on build stage

--- build-safari.sh
+++ build-safari.sh
@@ -2,23 +2,43 @@
 
 pushd .
 
-gulp_params=$1
+# store all params
+PARAMS=$@
+
+while (("$#")); do
+    case "$1" in
+        --serviceUrl=*)
+            serviceUrl="${1#*=}"
+            ;;
+    esac
+    shift
+done
 
 PATH=/usr/local/bin:$PATH
 
 npm i
-npx --no-install gulp build:safari ${gulp_params}
+npx --no-install gulp build:safari $PARAMS
 
 cd ./dist/safari
 
+appName=TMetric\ for\ Safari
+appExtensionName=TMetric\ for\ Safari\ Extension
+
+if [ -n "$serviceUrl" ]; then
+    search=https://app.tmetric.com
+    replace=$serviceUrl
+    file=./$appExtensionName/script.js
+    echo Replace $search with $replace in $file
+    awk -v a="$search" -v b="$replace" '{ gsub(a, b); print }' "$file" > tmp && mv tmp "$file"
+fi
+
 xcodebuild
 
-appName=TMetric\ for\ Safari
-infoPListFile=./${appName}/Info.plist
-version=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "${infoPListFile}")
+infoPListFile=./$appName/Info.plist
+version=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$infoPListFile")
 
-packageFile="tmetric_for_safari-${version}.pkg"
+packageFile="tmetric_for_safari-$version.pkg"
 
-pkgbuild --install-location /Applications --component "./build/Release/${appName}.app" "${packageFile}"
+pkgbuild --install-location /Applications --component "./build/Release/$appName.app" "$packageFile"
 
 popd
