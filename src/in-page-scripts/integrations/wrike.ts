47d3634e76c3ac76857e1d372a0c28ef20d10ca5
SergeyMul <sergeymul@devart.com>
2019-11-05 18:40:12 +0200

    TE-467 - added wrike dashboard

--- src/in-page-scripts/integrations/wrike.ts
+++ src/in-page-scripts/integrations/wrike.ts
@@ -23,6 +23,8 @@
             return;
         }
 
+        let issueTags = $$.all('.wspace-task-widgets-tags-dataview > div', issueElement);
+        let projectName = issueTags.length == 1 ? issueTags[0].textContent : null;
 
         let params = $$.searchParams(document.location.hash);
 
@@ -34,16 +36,62 @@
             issueId = inboxMatch[1];
         }
 
+        // get issue id from task in dashboard widgets
+        let isOverview = params['path'] == 'overview';
+        if (!issueId && isOverview) {
+
+            // find first task with matched issue and project name
+            // or just matched issue name
+            let taskInfo = $$.all('wrike-task-list-task')
+                // get task infos
+                .reduce((props, task) => {
+
+                    // skip task without id
+                    let id = task.getAttribute('data-id');
+                    if (!id) {
+                        return props;
+                    }
+
+                    // task element include task and subtasks
+                    // narrow to task
+                    let taskEl = $$('.task-block', task);
+
+                    // skip task with not matched name
+                    if ($$('wrike-task-title', taskEl).textContent != issueName) {
+                        return props;
+                    }
+
+                    // get project tags
+                    let tags = $$.all('.tag-text', taskEl);
+
+                    // skip if project tags more than 1
+                    if (tags.length > 1) {
+                        return props;
+                    }
+
+                    if (tags.length == 0) {
+                        // task from dashboard widget location root does not display project tag
+                        // include task without tag
+                        props.push({ id, projectMatch: false });
+                    } else if (tags[0].textContent == projectName) {
+                        // include task with matched project name
+                        props.push({ id, projectMatch: true });
+                    }
+
+                    return props;
+                }, <{ id: string, projectMatch: boolean }[]>[])
+                // prioritize tasks with matched project name
+                .sort((a, b) => (a.projectMatch ? 0 : 1) - (b.projectMatch ? 0 : 1))[0];
+
+            if (taskInfo) {
+                issueId = taskInfo.id;
+            }
+        }
+
         let issueUrl: string;
         if (issueId) {
             issueUrl = '/open.htm?id=' + issueId;
             issueId = '#' + issueId;
-        }
-
-        let issueTags = $$.all('.wspace-task-widgets-tags-dataview > div', issueElement);
-        let projectName: string;
-        if (issueTags.length == 1) {
-            projectName = issueTags[0].textContent;
         }
 
         let serviceType = 'Wrike';
