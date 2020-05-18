48555c722613b581fef03d21ae7108b29b771f5f
Artem Avramenko <artema@devart.com>
2020-05-18 19:09:13 +0300

    TE-540 - ignore service url for issues without external links

--- src/in-page-scripts/integrationService.ts
+++ src/in-page-scripts/integrationService.ts
@@ -381,6 +381,9 @@
     private static isSameIssue(oldIssue: WebToolIssue, newIssue: WebToolIssue) {
 
         function normalizeServiceUrl(issue: WebToolIssue) {
+            if (!issue.issueUrl) { // ignore service url for issue without external link (TE-540)
+                return '';
+            }
             let url = (issue.serviceUrl || '').trim();
             if (url.length && url[url.length - 1] == '/') {
                 return url.substring(0, url.length - 1);
