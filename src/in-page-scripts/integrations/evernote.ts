57f11d5cd009dc458ae39b3ae9edb1af77a123e2
Dmitriy Gryakov <dmitriygr@devart.com>
2020-05-25 15:40:33 +0300

    TE-531 - reworked getting issue name

--- src/in-page-scripts/integrations/evernote.ts
+++ src/in-page-scripts/integrations/evernote.ts
@@ -16,9 +16,22 @@
         const isIFrame = (input: HTMLElement | null): input is HTMLIFrameElement =>
             input !== null && input.tagName === 'IFRAME';
 
+        // try to get issue name from editor
         let frame = $$.try('#qa-COMMON_EDITOR_IFRAME');
         if (isIFrame(frame) && frame.contentDocument) {
-            var issueName = $$.try('en-noteheader', frame.contentDocument).innerText;
+            var issueName = ($$.try('en-noteheader textarea', frame.contentDocument) as HTMLTextAreaElement).value;
+        }
+
+        // try to get issue name from sidebar
+        if (!issueName) {
+            let sidebarSelectedItem = $$('.TSUJykWrzFUwd6gUIDTTl');
+            if (sidebarSelectedItem) {
+                issueName = $$.try('[id$=qa-NOTES_SIDEBAR_NOTE_TITLE]', sidebarSelectedItem).textContent;
+            }
+        }
+
+        if (issueName == '') {
+            issueName = 'Untitled';
         }
 
         if (!issueName) {
