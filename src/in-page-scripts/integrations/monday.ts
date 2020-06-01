d782990e7ea0319dd07723b5dffeecbe9a1ff01d
Dmitriy Gryakov <dmitriygr@devart.com>
2020-06-01 17:17:00 +0300

    TE-567 - got issueId on my week page

--- src/in-page-scripts/integrations/monday.ts
+++ src/in-page-scripts/integrations/monday.ts
@@ -11,6 +11,16 @@
         '.slide-panel-content',
         '#pulse-card-dialog-component'];
 
+    private static _lastClickedElement: HTMLElement;
+
+    constructor() {
+        document.addEventListener('click', this.saveClickedElement);
+    }
+
+    saveClickedElement(event: Event) {
+        Monday._lastClickedElement = event.target as HTMLElement;
+    }
+
     render(issueElement: HTMLElement, linkElement: HTMLElement) {
 
         if (issueElement.matches(this.issueElementSelector[0])) {
@@ -48,6 +58,7 @@
         let issueUrl;
         let projectName = $$('.board-name').textContent;
 
+        // board
         if (issueElement.matches(this.issueElementSelector[0])) {
             let colName = $$('[id$=-name].col-identifier-name', issueElement)
             if (colName) {
@@ -58,20 +69,56 @@
             }
         }
 
+        // side panel on board page
         if (issueElement.matches(this.issueElementSelector[1])) {
             issueName = $$.try('.title-wrapper', issueElement).textContent;
             issueId = source.path.match(/\/(?:pulses)\/([^\/]+)/)[1];
             issueUrl = source.path;
         }
 
+        // modal window on my week page
         if (issueElement.matches(this.issueElementSelector[2])) {
             issueName = $$.try('.pulse-name-value', issueElement).textContent;
             projectName = $$.try('.open-pulse-in-board-link').textContent;
+            if (Monday._lastClickedElement) {
+                let task = this.getTaskContentElement(Monday._lastClickedElement);
+                if (task) {
+                    let link = $$('.pulse-name-wrapper > a', task) as HTMLAnchorElement;
+
+                    issueId = link.pathname.match(/(?<=pulses\/)(\d*)(?=\/|\?|.*)/)[0];
+                    issueUrl = link.pathname;
+                }
+            }
+
+            if (!issueId) {
+                {
+                    let boardPath = ($$.try('.open-pulse-in-board-link', issueElement) as HTMLAnchorElement).pathname;
+                    let links = $$.all(`.pulse-name-wrapper a[href*="${boardPath}"]`);
+                    let link = links.find(link => link.textContent == issueName);
+                    if (link) {
+                        issueId = (<HTMLAnchorElement>link).pathname.match(/(?<=pulses\/)(\d*)(?=\/|\?|.*)/)[0];
+                        issueUrl = (<HTMLAnchorElement>link).pathname;
+                    }
+                }
+            }
         }
 
         let serviceUrl = source.protocol + source.host
 
         return { issueId, issueName, issueUrl, projectName, serviceUrl, serviceType: 'Monday' }
+    }
+
+    getTaskContentElement(el: HTMLElement): HTMLElement {
+
+        if (!el) {
+            return;
+        }
+
+        if (el.classList.contains('deadline-task-component-content')) {
+            return el;
+        }
+
+        return this.getTaskContentElement(el.parentElement);
     }
 }
 
