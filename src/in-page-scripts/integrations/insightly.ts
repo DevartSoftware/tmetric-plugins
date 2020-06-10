3262a9abcd7577fbe886bd1ca3eec90092af024c
Dmitriy Gryakov <dmitriygr@devart.com>
2020-06-10 19:44:25 +0300

    TE-573 - supported different places with task

--- src/in-page-scripts/integrations/insightly.ts
+++ src/in-page-scripts/integrations/insightly.ts
@@ -4,9 +4,9 @@
 
     observeMutations = true;
 
-    matchUrl = 'https://crm.*.insightly.com/list/Task/*';
+    matchUrl = [/https:\/\/crm\..*\.insightly.com\/.*\?blade=\/details\/task\/.*/i, /https:\/\/crm\..*\.insightly.com\/details\/task\/.*/i];
 
-    issueElementSelector = '#modal-details-body';
+    issueElementSelector = () => [$$('#main-container.details') || $$('#main-container.details.details-single')];
 
     render(issueElement: HTMLElement, linkElement: HTMLElement) {
         let toolbar = $$('.btn-toolbar.custom-buttons-toolbar', issueElement);
@@ -20,7 +20,7 @@
 
         let issueName = $$.try('#metadata-row-viewer-TITLE', issueElement).title;
         let issueId = $$.try('#metadata-row-viewer-TASK_ID', issueElement).title;
-        let issueUrl = issueId && `${source.path}?blade=/details/task/${issueId}`;
+        let issueUrl = issueId && `details/task/${issueId}`;
         let projectName = $$.try('#metadata-row-viewer-PROJECT_ID', issueElement).title ||
             $$.try('#metadata-row-viewer-OPPORTUNITY_ID', issueElement).title;
         let serviceUrl = source.protocol + source.host;
