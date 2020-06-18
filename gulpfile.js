c05b0d1845e44f039e8bcd2ad53d3ef6312f101e
Dmitry Andriyanov <dmitryan@devart.com>
2020-06-18 16:09:33 +0300

    TE-595 Fixed opening Settings page in Firefox

--- gulpfile.js
+++ gulpfile.js
@@ -318,7 +318,7 @@ function modifyManifestFirefox() {
                 gecko: { id: '@tmetric' }
             };
 
-            delete manifest['options_ui']['open_in_tab'];
+            //delete manifest['options_ui']['open_in_tab'];
 
             return manifest;
         }))
