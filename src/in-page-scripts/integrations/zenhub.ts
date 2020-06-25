ceddbbbf22cd7d4612298213bb5e168409946cdb
Dmitriy Gryakov <dmitriygr@devart.com>
2020-06-25 13:07:04 +0300

    TE-603 - fixed by changing issueElementSelector and selector for workspace searching

--- src/in-page-scripts/integrations/zenhub.ts
+++ src/in-page-scripts/integrations/zenhub.ts
@@ -6,7 +6,7 @@
 
     matchUrl = 'https://app.zenhub.com/workspaces/*/issues/*';
 
-    issueElementSelector = '.zhc-issue-meta';
+    issueElementSelector = ['.zhc-issue-modal', '.zh-workspace__container'];
 
     render(issueElement: HTMLElement, linkElement: HTMLElement) {
         let actions = $$('.zhc-issue-meta__actions', issueElement);
@@ -21,16 +21,16 @@
         let issueName: string;
         let issueId: string;
 
-        let issueTitle = $$.try('.zhc-issue-description__title').textContent;
+        let issueTitle = $$.try('.zhc-issue-description__title', issueElement).textContent;
         if (issueTitle) {
             let splittedTitle = issueTitle.split('#');
             issueName = splittedTitle[0];
             issueId = splittedTitle.length > 1 && splittedTitle[1];
         }
 
-        let projectName = $$.try('.zhc-workspace-header__name').textContent;
+        let projectName = $$.try('.zhc-breadcrumbs__button__name', issueElement).textContent;
         let issueUrl = source.path;
-        let serviceUrl = source.protocol + source.host
+        let serviceUrl = source.protocol + source.host;
 
         return { issueId, issueName, issueUrl, projectName, serviceUrl, serviceType: 'ZenHub' }
     }
