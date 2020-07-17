e36e3f002a658bfebca589c3e0af4587e56ab003
SergeyMul <sergeymul@devart.com>
2020-07-17 13:29:50 +0300

    v4.0.1

--- src/in-page-scripts/version.ts
+++ src/in-page-scripts/version.ts
@@ -15,7 +15,7 @@
     }
 
     let extensionInfo = { // object is updated from gulp build
-        version: '4.0.0'
+        version: '4.0.1'
     };
 
     let metaName = 'tmetric-extension-version';
