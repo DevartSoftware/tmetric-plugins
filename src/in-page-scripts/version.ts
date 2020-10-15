2248a41b765357d5de4b8354d81d96dde06e2127
ArtemA <artema@devart.com>
2020-10-15 18:19:54 +0300

    TE-679 - fixed task links

--- src/in-page-scripts/version.ts
+++ src/in-page-scripts/version.ts
@@ -15,7 +15,7 @@
     }
 
     let extensionInfo = { // object is updated from gulp build
-        version: '4.0.4'
+        version: '4.0.5'
     };
 
     let metaName = 'tmetric-extension-version';
