1dc10df3140d5dda9b8e2c16f6e446eed5071aeb
SergeyMul <sergeymul@devart.com>
2020-12-28 14:05:12 +0200

    fixed linter errors

--- src/in-page-scripts/integrations/wrike.ts
+++ src/in-page-scripts/integrations/wrike.ts
@@ -9,7 +9,7 @@
     issueElementSelector = '.wspace-task-view';
 
     render(issueElement: HTMLElement, linkElement: HTMLElement) {
-        let host = $$('.wrike-panel-header-toolbar, .action-bar', issueElement);
+        const host = $$('.wrike-panel-header-toolbar, .action-bar', issueElement);
         if (host) {
             linkElement.classList.add('devart-timer-link-wrike');
             host.insertBefore(linkElement, host.firstElementChild);
@@ -18,30 +18,30 @@
 
     getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
 
-        let issueName = $$.try<HTMLTextAreaElement>('textarea.title-field, textarea.title__field', issueElement).value;
+        const issueName = $$.try<HTMLTextAreaElement>('textarea.title-field, textarea.title__field', issueElement).value;
         if (!issueName) {
             return;
         }
 
-        let issueTags = $$.all('.wspace-task-widgets-tags-dataview > div, .task-tags .tag-text', issueElement);
-        let projectName = issueTags.length == 1 ? issueTags[0].textContent : null;
+        const issueTags = $$.all('.wspace-task-widgets-tags-dataview > div, .task-tags .tag-text', issueElement);
+        const projectName = issueTags.length == 1 ? issueTags[0].textContent : null;
 
-        let params = $$.searchParams(document.location.hash);
+        const params = $$.searchParams(document.location.hash);
 
         let issueId = params['t']                 // folder, My Work,
             || params['ot'];                      // modal
 
-        let inboxMatch = document.location.hash && document.location.hash.match(/#\/inbox\/task\/(\d+)/);
+        const inboxMatch = document.location.hash && document.location.hash.match(/#\/inbox\/task\/(\d+)/);
         if (inboxMatch) {
             issueId = inboxMatch[1];
         }
 
         // get issue id from task in dashboard widgets
-        let isOverview = params['path'] == 'overview';
+        const isOverview = params['path'] == 'overview';
         if (!issueId && isOverview) {
 
             // find issue identifier by name
-            let foundIdentifiers = $$.all('wrike-task-list-task')
+            const foundIdentifiers = $$.all('wrike-task-list-task')
                 .map(task => {
                     if ($$('.task-block wrike-task-title', task).textContent == issueName) {
                         return task.getAttribute('data-id');
@@ -63,9 +63,9 @@
             issueId = '#' + issueId;
         }
 
-        let serviceType = 'Wrike';
+        const serviceType = 'Wrike';
 
-        let serviceUrl = source.protocol + source.host;
+        const serviceUrl = source.protocol + source.host;
 
         return { issueId, issueName, projectName, serviceType, serviceUrl, issueUrl };
     }
