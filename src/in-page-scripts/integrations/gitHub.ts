acbeaba1ac9add5ffd71be2ad6c4e06d49a9513c
SergeyMul <sergeymul@devart.com>
2021-01-14 13:27:30 +0200

    TE-695 - updated tag selector for GitHub

--- src/in-page-scripts/integrations/gitHub.ts
+++ src/in-page-scripts/integrations/gitHub.ts
@@ -35,7 +35,7 @@
         let issueId = match[4];
         issueId = (issueType == 'pull' ? '!' : '#') + issueId
         const serviceType = 'GitHub';
-        const tagNames = $$.all('.sidebar-labels .sidebar-labels-style').map(label => label.textContent);
+        const tagNames = $$.all('.sidebar-labels .IssueLabel').map(label => label.textContent);
 
         return { issueId, issueName, projectName, serviceType, serviceUrl, issueUrl, tagNames };
     }
