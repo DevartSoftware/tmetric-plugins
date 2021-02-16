4c600f94840ed623d56eacc870444a964f7d2a5f
Alexander Shymchenko <alexandersh@devart.com>
2021-02-16 16:49:38 +0200

    TE-517 show informative message from API in alert

--- src/background/oidcClient.ts
+++ src/background/oidcClient.ts
@@ -154,7 +154,7 @@
                 let statusText = xhr.statusText;
                 let responseMessage: string;
                 if (xhr.responseJSON) {
-                    responseMessage = xhr.responseJSON.message;
+                    responseMessage = xhr.responseJSON.Message;
                 }
 
                 if (statusText == 'error') // jQuery replaces empty status to 'error'
