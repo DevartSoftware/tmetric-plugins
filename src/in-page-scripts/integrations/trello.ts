2a1dec297ef592c978fbe8ee322ef630e836dfab
SergeyMul <sergeymul@devart.com>
2021-08-27 19:06:57 +0300

    handled linter errors

--- src/in-page-scripts/integrations/trello.ts
+++ src/in-page-scripts/integrations/trello.ts
@@ -15,7 +15,7 @@
 
         if (issueElement.matches(this.issueElementSelector[0])) {
             // cut 'timer' so that time can be visible if we have time
-            let text = linkElement.lastElementChild.textContent;
+            const text = linkElement.lastElementChild.textContent;
             if (/[0-9]/.test(text)) {
                 linkElement.lastElementChild.textContent = text.replace(' timer', '');
             }
@@ -23,7 +23,7 @@
             linkElement.classList.add('button-link');
             issueElement.insertBefore(linkElement, issueElement.firstElementChild);
         } else if (issueElement.matches(this.issueElementSelector[1])) { // for checklist
-            let wrapper = $$('.checklist-item-controls', issueElement);
+            const wrapper = $$('.checklist-item-controls', issueElement);
             if (wrapper) {
                 linkElement.classList.add('devart-timer-link-minimal', 'devart-timer-link-trello');
                 wrapper.appendChild(linkElement);
@@ -37,20 +37,20 @@
         // https://trello.com/c/CARD_ID/CARD_NUMBER-CARD_TITLE_DASHED_AND_LOWERCASED
         // Effective card url:
         // https://trello.com/c/CARD_ID
-        var match = /^\/c\/(.+)\/(\d+)-(.+)$/.exec(source.path);
+        const match = /^\/c\/(.+)\/(\d+)-(.+)$/.exec(source.path);
         if (!match) {
             return;
         }
 
         // match[2] is a 'CARD_NUMBER' from path
-        var issueId = match[2];
+        let issueId = match[2];
         if (!issueId) {
             return;
         }
         issueId = '#' + issueId;
 
         // <h2 class="window-title-text current hide-on-edit js-card-title">ISSUE_NAME</h2>
-        var issueName = $$.try('.window-title h2').textContent;
+        const issueName = $$.try('.window-title h2').textContent;
         if (!issueName) {
             return;
         }
@@ -58,16 +58,15 @@
         //  <a class="board-header-btn board-header-btn-name js-rename-board" href="#">
         //    <span class="board-header-btn-text">Test Board</span>
         //  </a>
-        var projectName = $$.try('.board-header-btn > .board-header-btn-text').textContent;
+        const projectName = $$.try('.board-header-btn > .board-header-btn-text').textContent;
 
-        var serviceUrl = source.protocol + source.host;
+        const serviceUrl = source.protocol + source.host;
 
-        var issueUrl = '/c/' + match[1];
+        const issueUrl = '/c/' + match[1];
 
-        var tagNames = $$.all('.js-card-detail-labels-list .card-label').map(label => label.textContent);
+        const tagNames = $$.all('.js-card-detail-labels-list .card-label').map(label => label.textContent);
 
         let description: string;
-
         if (issueElement.matches(this.issueElementSelector[1])) {
             description = $$.try('.checklist-item-details-text', issueElement).textContent;
         }
