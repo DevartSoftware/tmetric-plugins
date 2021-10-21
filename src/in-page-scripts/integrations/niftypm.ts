aed4a3dcc3325ecd8a96f380705818f672d21781
technowand <parasgpm@gmail.com>
2021-10-21 10:10:39 +0530

    integrate niftyPM

---
+++ src/in-page-scripts/integrations/niftypm.ts
@@ -0,0 +1,52 @@
+﻿class NiftyPM implements WebToolIntegration {
+
+    matchUrl = '*://*.niftypm.com/*';
+
+    issueElementSelector = '.content-panel-controls';
+
+    observeMutations = true;
+
+
+    /**
+     * Extracts information about the issue (ticket or task) from a Web
+     * page by querying the DOM model.
+     */
+    getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
+        let issueId = issueElement.getAttribute('data-issue-id');
+        let issueName = issueElement.getAttribute('data-issue-name');
+        let serviceUrl = issueElement.getAttribute('data-service-url');
+        let issueUrl = issueElement.getAttribute('data-issue-url');
+        let projectName = issueElement.getAttribute('data-project-name');
+
+        let tagNames: string[];
+        let tagNamesAttribute = issueElement.getAttribute('data-tag-names');
+        if (tagNamesAttribute) {
+            tagNames = tagNamesAttribute.split(',');
+        }
+
+    return {
+            issueId,
+            issueName,
+            issueUrl,
+            projectName,
+            serviceUrl,
+            serviceType: 'NiftyPM',
+            tagNames
+        };
+    }
+
+    /**
+     * Inserts the timer button for the identified issue into a Web page.
+     */
+    render(issueElement: HTMLElement, linkElement: HTMLElement) {
+        issueElement.appendChild(linkElement);
+        // const trackBtn = $$('.content-panel-simple-action', issueElement);
+        //
+        // if (trackBtn) {
+        //     linkElement.classList.add('timer-link-niftypm');
+        //     trackBtn.before(linkElement);
+        // }
+    }
+}
+
+IntegrationService.register(new NiftyPM());
