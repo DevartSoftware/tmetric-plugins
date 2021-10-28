00a5c52af4c5384b13132be4cf68747498126544
SergeyMul <sergeymul@devart.com>
2021-10-28 13:01:29 +0300

    TMET-7689 - added link to all tasks and my tasks pages in Nifty

--- src/in-page-scripts/integrations/nifty.ts
+++ src/in-page-scripts/integrations/nifty.ts
@@ -1,10 +1,10 @@
 ﻿class Nifty implements WebToolIntegration {
 
-    matchUrl = '*://*.nifty.pm/*/task/*';
+    matchUrl = '*://*.nifty.pm/*';
 
     issueElementSelector = [
-        '.content-panel:not(.content-panel-subview)',
-        '.content-panel .subtask-item:not(.new)'
+        '.content-panel:not(.content-panel-subview)', // task
+        '.content-panel .subtask-item:not(.new)' // subtask
     ];
 
     showIssueId = true;
@@ -16,15 +16,55 @@
         let description = '';
         if (issueElement.matches(this.issueElementSelector[1])) {
             description = $$.try('.item-title', issueElement).textContent;
+            // search parent task for subtask
             issueElement = $$.closest(this.issueElementSelector[0], issueElement);
         }
 
         const issueId = $$.try('.nice-id', issueElement).textContent;
-        const issueName = $$.try('.content-panel-field-input', issueElement).textContent;
-        const projectName = $$.try('.header-title h1').textContent;
+        if (!issueId) {
+            return;
+        }
 
-        const serviceUrl = source.fullUrl.split('task')[0];
-        const issueUrl = `task/${issueId}`;
+        const issueName = $$.try('.content-panel-field-input', issueElement).textContent;
+
+        const serviceUrl = source.protocol + source.host;
+
+        let projectId = '';
+        let projectName = '';
+
+        // match url for project home page or project milestones page or project tasks page with opened task
+        const projectPageMatch = source.path.match(/\/([^\/]+)\/(home|roadmap|task\/.+)/);
+        if (projectPageMatch) {
+            projectId = projectPageMatch[1];
+            projectName = $$.try('.header-title h1').textContent;
+        }
+
+        // match url for all tasks page or my tasks page
+        const tasksPageMatch = source.path.match(/\/(my\/)?tasks/);
+        if (tasksPageMatch) {
+
+            // find task element where task name or parent task name match issue name
+            const task = $$('.task-name', null, element => {
+                return $$.all('.task-name-text, .task-task-name', element).some(element => {
+                    return element.textContent.trim() == issueName.trim();
+                });
+            });
+
+            // find project element inside found task element
+            if (task) {
+                const project = $$('.task-project-name', task) as HTMLAnchorElement;
+                if (project) {
+                    projectId = $$.getRelativeUrl(serviceUrl, project.href);
+                    projectName = project.textContent;
+                }
+            }
+        }
+
+        if (!projectId) {
+            return;
+        }
+
+        const issueUrl = `${projectId}/task/${issueId}`;
 
         const tagNames = $$.all('.labels-list-item-text', issueElement).map(_ => _.textContent);
 
