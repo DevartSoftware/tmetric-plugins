a545c842a3bd49219695b1e5f4ccef061d5f70d4
ViktorF <ViktorF@devart.com>
2023-03-21 16:50:24 +0200

    TMET-8864. Fixed task name if it not specified

--- src/in-page-scripts/integrations/ticktick.ts
+++ src/in-page-scripts/integrations/ticktick.ts
@@ -31,13 +31,16 @@
     }
 
     getIssue(issueElement: HTMLElement, source: Source): WebToolIssue {
-        const issueName =
-            $$.try('.CodeMirror-code span', issueElement).textContent ||
-                $$.try('.title > span', issueElement).textContent;
+        let issueNameElement =
+            $$.try('.CodeMirror-code span', issueElement).textContent
+                ? $$.try('.CodeMirror-code span', issueElement)
+                : $$.try('.title > span', issueElement);
+        let issueName = issueNameElement.textContent
         if (!issueName) {
             return;
         }
-
+        const isGrayed = [...issueNameElement.classList].some(x => /text-gr[ae]y-[1-5]0/.test(x));
+        issueName = isGrayed ? "" : issueName
         // get identifier from href or from top task in single view
         let issueId: string;
         let issueUrl: string;
