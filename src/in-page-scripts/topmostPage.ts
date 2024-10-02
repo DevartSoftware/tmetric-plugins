46a1bf62ab293b8d6d500157c7ecc234e81c99b7
ArtemA <artema@devart.com>
2024-10-02 15:37:53 +0300

    TMET-10630 - fixed dark mode

--- src/in-page-scripts/topmostPage.ts
+++ src/in-page-scripts/topmostPage.ts
@@ -42,7 +42,8 @@ if (typeof document !== 'undefined') {
                 left: 0,
                 width: '100%',
                 height: '100%',
-                display: 'block'
+                display: 'block',
+                colorScheme: 'normal'
             });
         }
 
