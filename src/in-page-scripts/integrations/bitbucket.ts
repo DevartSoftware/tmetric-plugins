c4421cabed1e527d555c686922f89d9fdc31512a
ArtemA <artema@devart.com>
2024-11-26 18:19:11 +0200

    TMET-10812 - support Bitbucket commits

--- src/in-page-scripts/integrations/bitbucket.ts
+++ src/in-page-scripts/integrations/bitbucket.ts
@@ -2,10 +2,10 @@ class Bitbucket implements WebToolIntegration {
 
     showIssueId = true;
 
-    matchUrl = [
-        '*://*/issues/*',
-        '*://*/pull-requests/*'
-    ];
+    // https://bitbucket.org/WORKSPACE/REPO/issues/123
+    // https://bitbucket.org/WORKSPACE/REPO/pull-requests/123
+    // https://bitbucket.org/WORKSPACE/REPO/commits/abcdef123456abcdef123456abcdef12345abcde
+    matchUrl = /^([^:]+:\/\/[^\/]+)(.+)\/(issues|pull-requests|commits)\/([^\/\?]+)/;
 
     match() {
         return $$.getAttribute('meta[name=application-name]', 'content') == 'Bitbucket';
@@ -15,58 +15,75 @@ class Bitbucket implements WebToolIntegration {
 
         const issueToolbar = $$('#issue-header .issue-toolbar');
         if (issueToolbar) {
-            const linkContainer = $$.create('div', 'devart-timer-link-bitbucket', 'aui-buttons')
+            const linkContainer = $$.create('div', 'aui-buttons')
             linkElement.classList.add('aui-button');
+            linkElement.style.marginRight = '10px'
             linkContainer.appendChild(linkElement);
             issueToolbar.insertBefore(linkContainer, issueToolbar.firstElementChild);
             return;
         }
 
-        const pullRequestActionPanel = $$('main [data-qa=pr-header-actions-drop-down-menu-styles]')?.parentElement;
-        if (pullRequestActionPanel) {
-            linkElement.style.marginTop = '6px';
-            pullRequestActionPanel.prepend(linkElement);
+        const toolbar =
+            $$('main [data-qa=pr-header-actions-drop-down-menu-styles]')?.parentElement || // pull request actions
+            $$('button[data-testid="commit-more-button--trigger"]')?.parentElement; // commit actions
+        if (toolbar) {
+            Object.assign(linkElement.style, {
+                marginRight: '6px',
+                marginTop: '6px'
+            } as CSSStyleDeclaration);
+            toolbar.prepend(linkElement);
             return;
         }
 
-        let pullRequestHeading = $$('main h1');
-        if (pullRequestHeading) {
-            pullRequestHeading.parentElement!.appendChild(linkElement);
+        let fallbackHeading = $$('main h1');
+        if (fallbackHeading) {
+            fallbackHeading.parentElement!.appendChild(linkElement);
         }
     }
 
     getIssue(_issueElement: HTMLElement, source: Source) {
 
-        // https://bitbucket.org/NAMESPACE/TRANSFORMED_PROJECT_NAME/issues/NUMBER/TRANSFORMED_ISSUE_NAME
-        // https://bitbucket.org/NAMESPACE/TRANSFORMED_PROJECT_NAME/pull-requests/NUMBER/TRANSFORMED_PULL_REQUEST_NAME/VIEW
-        const match = /^(.+)\/(issues|pull-requests)\/(\d+).*$/.exec(source.path);
-
+        const match = this.matchUrl.exec(source.fullUrl);
         if (!match) {
             return;
         }
 
-        // match[3] is a 'NUMBER' from path
-        const issueNumber = match[3];
-        if (!issueNumber) {
+        // '', 'https: //bitbucket.org' '/WORKSPACE/REPO', 'pull-requests', '123'
+        let [, serviceUrl, repoPath, issueType, issueNumber] = match;
+
+        if (issueType == 'commits') {
+
+            // issueNumber is commit hash
+            issueNumber = issueNumber.toLowerCase();
+            if (/[^a-f0-9]/.test(issueNumber)) {
+                return;
+            }
+
+            // https://bitbucket.org/WORKSPACE/REPO/src/abcdef123456abcdef123456abcdef12345abcde/
+            const hrefPattern = `/src/${issueNumber}`;
+            const href = $$<HTMLAnchorElement>(`a[href*="${hrefPattern}"]`)?.href;
+            if (href) {
+                const i = href.indexOf(hrefPattern);
+                issueNumber = href!.substring(i).match(/\/src\/([a-f0-9]+)/)![1];
+            }
+        } else if (/\D/.test(issueNumber)) {
             return;
         }
+
+        const issueUrl = `${repoPath}/${issueType}/${issueNumber}`;
 
         let issueId: string | undefined;
         let issueName: string | undefined | null;
 
-        const issueType = match[2];
         if (issueType == 'issues') {
             issueId = '#' + issueNumber;
-
-            // <h1 id="issue-title">ISSUE_NAME</h1>
             issueName = $$.try('#issue-title').textContent;
         } else if (issueType == 'pull-requests') {
             issueId = '!' + issueNumber;
-
-            // <h1 class="...">
-            //     PULL_REQUEST_NAME
-            // </span>
-            issueName = $$.try('h1').textContent;
+            issueName = $$.try('main h1').textContent;
+        } else if (issueType == 'commits') {
+            issueId = issueNumber.substring(0, 7);
+            issueName = 'Commit ' + issueNumber.substring(0, 12);
         }
 
         if (!issueName) {
@@ -75,22 +92,12 @@ class Bitbucket implements WebToolIntegration {
 
         // <a href="/account/user/almtoolsteam/projects/CR">Code Review</a>
         const projectName = $$.try(
-            '.aui-nav-breadcrumbs a, header a',
+            'main nav a',
             null,
             el => /.+\/projects\/.+/.test(el.getAttribute('href')!)
         ).textContent;
 
         const serviceType = 'Bitbucket';
-
-        // match[1] is a 'https://bitbucket.org/NAMESPACE/TRANSFORMED_PROJECT_NAME' from path
-        // cut '/NAMESPACE/TRANSFORMED_PROJECT_NAME' from path
-        let servicePath = match[1].split('/').slice(0, -2).join('/');
-        servicePath = (servicePath) ? '/' + servicePath : '';
-
-        const serviceUrl = source.protocol + source.host + servicePath;
-
-        const issueUrl = match[1].split('/').slice(-2).join('/') + '/' + issueType + '/' + issueNumber;
-
         return {
             issueId, issueName, projectName, serviceType, serviceUrl, issueUrl
         } as WebToolIssue;
