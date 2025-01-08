8cb387b8cee31c6c0f261cb9451728c6fc9d2af7
ArtemA <artema@devart.com>
2025-01-08 16:30:17 +0200

    TMET-10899 - support GitHub projects

--- setVersion.bat
+++ setVersion.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d %~dp0
 
-call npx --no-install gulp version --newversion=5.0.6
+call npx --no-install gulp version --newversion=5.0.7
--- src/in-page-scripts/integrations/gitHub.ts
+++ src/in-page-scripts/integrations/gitHub.ts
@@ -2,20 +2,37 @@ class GitHub implements WebToolIntegration {
 
     showIssueId = true;
 
-    matchUrl = /(https:\/\/github\.com)(\/.+\/(issues|pull)\/(\d+))/
+    matchUrl = [
+        /(https:\/\/github\.com)(\/.+\/(issues|pull)\/(\d+))/, // classic view
+        /(https:\/\/github\.com)\/.+[&\?]issue=([^&]+)/ // project view (TMET-10899)
+    ]
 
     render(_issueElement: HTMLElement, linkElement: HTMLElement) {
+
+        linkElement.classList.add('mr-2'); // margin for edit button
+        linkElement.classList.add('btn');
+        linkElement.classList.add('btn-sm');
+
         const host = $$('.gh-header-actions');
         if (host) {
             linkElement.style.display = 'inline-block'; // ZenHub hides action links by default
-            linkElement.classList.add('mr-2'); // margin for edit button
-            linkElement.classList.add('btn');
-            linkElement.classList.add('btn-sm');
             host.insertBefore(linkElement, host.firstElementChild);
+            return;
+        }
+
+        // project view
+        const firstHeaderButton = $$.visible('div[data-testid="issue-header"] button');
+        if (firstHeaderButton) {
+            firstHeaderButton.parentElement!.insertBefore(linkElement, firstHeaderButton);
         }
     }
 
     getIssue(_issueElement: HTMLElement, source: Source) {
+
+        const match = this.matchUrl[0].exec(source.fullUrl);
+        if (!match) {
+            return this.getProjectIssue(_issueElement, source);
+        }
 
         const issueName = $$.try('.js-issue-title').textContent;
         if (!issueName) {
@@ -27,14 +44,45 @@ class GitHub implements WebToolIntegration {
 
         // https://github.com/NAMESPACE/PROJECT/issues/NUMBER
         // https://github.com/NAMESPACE/PROJECT/pull/NUMBER
-        const match = this.matchUrl.exec(source.fullUrl)!;
         const serviceUrl = match[1];
         const issueUrl = match[2];
         const issueType = match[3];
         let issueId = match[4];
         issueId = (issueType == 'pull' ? '!' : '#') + issueId
         const serviceType = 'GitHub';
-        const tagNames = $$.all('.js-issue-labels .IssueLabel').map(label => label.textContent);
+        const tagNames = $$.all('.js-issue-labels .IssueLabel')
+            .map(label => label.textContent);
+
+        return {
+            issueId, issueName, projectName, serviceType, serviceUrl, issueUrl, tagNames
+        } as WebToolIssue;
+    }
+
+    getProjectIssue(_issueElement: HTMLElement, source: Source) {
+
+        let issueId: string | undefined;
+        let issueUrl: string | undefined;
+        const issueName = $$('h2 [data-testid="issue-title"]')?.textContent;
+        if (!issueName) {
+            return;
+        }
+
+        const match = this.matchUrl[1].exec(source.fullUrl)!;
+        const serviceUrl = match[1];
+        const issueParams = decodeURIComponent(match[2]).split('|'); // "MyNamespace|MyProject|123"
+        if (issueParams.length === 3) {
+            issueId = '#' + issueParams[2];
+            issueUrl = ['', ...issueParams.slice(0, 2), 'issues', issueParams[2]]
+                .map(s => encodeURIComponent(s))
+                .join('/');
+        }
+
+        const serviceType = 'GitHub';
+        const projectName = $$<HTMLAnchorElement>('.AppHeader-context-full ul > li a',
+            null,
+            el => !!el.href.match(/a/))?.textContent;
+        const tagNames = $$.all('div[data-testid="issue-labels"] > a > span:not(.sr-only):first-of-type')
+            .map(label => label.textContent);
 
         return {
             issueId, issueName, projectName, serviceType, serviceUrl, issueUrl, tagNames
