99dc6dc340b529536782afdcb53ac090132a868b
ArtemA <artema@devart.com>
2025-01-23 10:57:11 +0200

    Keep errorlevel

--- build.bat
+++ build.bat
@@ -5,10 +5,13 @@ pushd "%~dp0"
 cd /d %~dp0
 
 call npm install
+IF %ERRORLEVEL% NEQ 0 GOTO END
 
 call npx --no-install gulp %*
+IF %ERRORLEVEL% NEQ 0 GOTO END
 
 :: Pack sources for Mozilla
 git archive -o dist/firefox/tmetric-src.zip HEAD
 
+:END
 popd
