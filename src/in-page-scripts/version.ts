6bd9b55cdca5a247d6a41f9a055521f7de4190b8
ArtemA <artema@devart.com>
2025-03-11 17:12:50 +0200

    v5.0.11

--- src/in-page-scripts/version.ts
+++ src/in-page-scripts/version.ts
@@ -37,7 +37,7 @@
     }
 
     const extensionInfo = { // object is updated from gulp build
-        version: '5.0.10'
+        version: '5.0.11'
     };
 
     addMeta('tmetric-extension-version', extensionInfo.version);
