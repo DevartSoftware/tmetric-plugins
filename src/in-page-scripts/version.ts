42dc054eef229362534938e264b3fcb4db5ea97f
Dmitriy Gryakov <dmitriygr@devart.com>
2025-07-08 11:05:04 +0300

    v5.0.16

--- src/in-page-scripts/version.ts
+++ src/in-page-scripts/version.ts
@@ -37,7 +37,7 @@
     }
 
     const extensionInfo = { // object is updated from gulp build
-        version: '5.0.15'
+        version: '5.0.16'
     };
 
     addMeta('tmetric-extension-version', extensionInfo.version);
