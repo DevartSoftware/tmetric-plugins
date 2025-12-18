185423305aeb5f5124f5713a0d59a0e7e9087d0a
Dmitriy Gryakov <dmitriygr@devart.com>
2025-12-18 11:32:21 +0200

    TMET-12671 - Не отображается Start timer в bitrix24
    https://devart.atlassian.net/browse/TMET-12671

--- src/in-page-scripts/integrations/bitrix24.ts
+++ src/in-page-scripts/integrations/bitrix24.ts
@@ -6,32 +6,17 @@ class Bitrix24 implements WebToolIntegration {
 
     render(_issueElement: HTMLElement, linkElement: HTMLElement) {
 
-        const host = $$('[data-bx-id="task-view-b-buttonset"]');
-        if (host) {
-            linkElement.classList.add('webform-small-button', 'webform-small-button-transparent')
-            host.appendChild(linkElement);
+        const title = $$('.tasks-field-title');
+        if (title) {
+            linkElement.classList.add('devart-timer-link-bitrix24')
+            title.parentNode?.insertBefore(linkElement, title.nextSibling);
         }
     }
 
     getIssue(_issueElement: HTMLElement, source: Source) {
 
-        // get issue name (TMET-10815)
-        let issueName: string | null = '';
-        const issueNameElement = $$('#pagetitle');
-        if (issueNameElement?.childNodes) {
-            for (let node of issueNameElement.childNodes) {
-                if (/task-popup-pagetitle-item/.test((node as Element).className)) {
-                    issueName = node.textContent;
-                    break;
-                }
-                if (!/actions/.test((node as Element).className)) {
-                    if (issueName) {
-                        issueName += ' ';
-                    }
-                    issueName += (node.textContent || '').trim();
-                }
-            }
-        }
+        let issueName = $$<HTMLTextAreaElement>('.tasks-field-title > textarea')?.value ||
+            $$('.tasks-field-title')?.getAttribute('data-task-field-value');
 
         if (!issueName) {
             return;
@@ -50,7 +35,7 @@ class Bitrix24 implements WebToolIntegration {
         const serviceUrl = source.protocol + source.host;
         const serviceType = 'Bitrix24';
 
-        const projectName = $$.try('.task-detail-extra .task-group-field').textContent;
+        const projectName = $$.try('.tasks-field-group-title').textContent;
 
         return {
             issueId, issueName, issueUrl, serviceUrl, projectName, serviceType
