a53074ae434a4b74bbb9ed1ce93bc3c10542fbc7
Dmitriy Gryakov <dmitriygr@devart.com>
2025-12-18 13:34:26 +0200

    v5.0.21

--- setVersion.bat
+++ setVersion.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d %~dp0
 
-call npx --no-install gulp version --newversion=5.0.22
+call npx --no-install gulp version --newversion=5.0.21
--- src/in-page-scripts/version.ts
+++ src/in-page-scripts/version.ts
@@ -37,7 +37,7 @@
     }
 
     const extensionInfo = { // object is updated from gulp build
-        version: '5.0.22'
+        version: '5.0.21'
     };
 
     addMeta('tmetric-extension-version', extensionInfo.version);
