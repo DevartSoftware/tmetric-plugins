78fc40bbd00e7ef46cc52565f2d5b4b2b7428aab
Dmitriy Gryakov <dmitriygr@devart.com>
2026-03-16 20:57:24 +0200

    TMET-13094 - fixed by supporting v4

--- src/in-page-scripts/integrations/clickup.ts
+++ src/in-page-scripts/integrations/clickup.ts
@@ -5,43 +5,32 @@ class Clickup implements WebToolIntegration {
     matchUrl = '*://app.clickup.com';
 
     issueElementSelector = [
-        '.task',
-        '.lv-subtask__outer', // subtask list item
-        '.checklist2-row', // check list item
-        '.cu-task-view__container',
-        '.cu-task-row'
+        '.cu-checklist-item__row', // v3 v4 check list item
+        '.cu-task-view__container', // v3 task view
+        '.cu-task-row', // v3 v4 tasks list
+        '.cu-task-view__main' // v4 task view
     ];
 
     render(issueElement: HTMLElement, linkElement: HTMLElement) {
         linkElement.classList.add('devart-timer-link-clickup');
+
         if (issueElement.matches(this.issueElementSelector[0])) {
-            let element = $$('.task__toolbar:nth-last-of-type(1) .task__toolbar-container', issueElement); // v 2.0
-            if (element) {
-                element.insertBefore(linkElement, element.lastElementChild);
-            } 
-        } else if (issueElement.matches(this.issueElementSelector[1])) {
             linkElement.classList.add('devart-timer-link-minimal');
-            let element = $$('.task-todo-item__name-text .task-todo-item__actions', issueElement); // v 2.0
-            if (element) {
-                element.parentElement!.insertBefore(linkElement, element);
-            }
-        } else if (issueElement.matches(this.issueElementSelector[2])) {
-            linkElement.classList.add('devart-timer-link-minimal');
-            let element = $$('.checklist2-row-item-name', issueElement); // v 2.0
+            let element = $$('.cu-checklist-item__name', issueElement);
             if (element) {
                 element.parentElement!.insertBefore(linkElement, element.nextElementSibling);
             } 
-        } else if (issueElement.matches(this.issueElementSelector[3])) {
-            let element = $$('.cu-task-view-header__right.ng-star-inserted', issueElement); // v 3.0
+        } else if (issueElement.matches(this.issueElementSelector[1]) || issueElement.matches(this.issueElementSelector[3])) {
+            let element = $$('.cu-task-view-header__right', issueElement);
             if (element) {
                 element.insertBefore(linkElement, element.firstElementChild);
             } 
-        } else if (issueElement.matches(this.issueElementSelector[4])) {
-            let element = $$('.cu-task-row-main__link', issueElement); // v 3.0
+        } else if (issueElement.matches(this.issueElementSelector[2])) {
+            let element = $$('.cu-task-row-main__actions', issueElement);
             if (element) {
                 element.appendChild(linkElement);
             }
-            element = $$('.cu-task-row__actions', issueElement); // v 3.0
+            element = $$('.cu-task-row__actions', issueElement);
             if (element) {
                 element.insertBefore(linkElement, element.firstElementChild);
             }
@@ -55,7 +44,7 @@ class Clickup implements WebToolIntegration {
         const serviceUrl = source.protocol + source.host;
 
         let issueId = issueElement.getAttribute('data-id');
-        if (!issueId) {
+        if (!issueId || issueElement.matches(this.issueElementSelector[0])) {
             const matches = source.fullUrl.match(/\/t\/([^\/]+)$/);
             if (matches) {
                 issueId = matches[1];
@@ -67,43 +56,26 @@ class Clickup implements WebToolIntegration {
             }
         }
 
-        let issueName = $$.try('.task-name__overlay').textContent
-            || $$.try('.cu-task-title__overlay', issueElement).textContent // v3.0 - task detail page
-            || $$.try('.cu-task-row-main__link-text-inner', issueElement).textContent; // v3.0 - tasks list
+        let issueName = $$.try('.cu-task-title__overlay', issueElement).textContent // task detail page
+            || $$.try('.cu-task-row-main__link-text-inner', issueElement).textContent // tasks list
+            || $$.try('cu-task-hero-section .cu-task-title__overlay').textContent // checklist-item
 
-        let tags = $$.all('.cu-tags-view__container .cu-tags-view .cu-tags-select__name', issueElement);
-
-        if (tags.length == 0 && issueElement.matches(this.issueElementSelector[3])) { // v3.0 - task detail page
-            tags = $$.all('cu-task-hero-section .cu-tags-select__name', issueElement);
-            if (tags.length == 0) {
-                tags = $$.all('.cu-task-hero-section__row-item .cu-tags-select__name', issueElement); 
-            }
-        }
-        if (tags.length == 0 && issueElement.matches(this.issueElementSelector[4])) {
-            tags = $$.all('.cu-tags-view__item .cu-tags-select__name', issueElement); // v3.0 - tasks list
+        let tags = $$.all('cu-tags-list .cu-tags-select__name', issueElement);
+        if (tags.length == 0 && issueElement.matches(this.issueElementSelector[0])) {
+            tags = $$.all('cu-task-hero-section .cu-tags-select__name');
         }
 
         let description: string | null | undefined;
-        if (issueElement.matches(this.issueElementSelector[1])) {
-            const subtaskLink = $$('.task-todo-item__name-text a', issueElement) as HTMLAnchorElement;
-            if (subtaskLink) {
-                const matches = subtaskLink.href.match(/\/t\/([^\/]+)$/);
-                if (matches) {
-                    issueName = subtaskLink.textContent;
-                    issueId = matches[1];
-                    tags = $$.all('.cu-tags-view__container-list .cu-tags-view .cu-tags-select__name', issueElement);
-                }
-            }
-        } else if (issueElement.matches(this.issueElementSelector[2])) {
-            description = $$.try('.checklist2-row-item-name > p, :scope > p', issueElement).textContent; // v2.0, v1.0
+        if (issueElement.matches(this.issueElementSelector[0])) {
+            description = $$.try('.cu-checklist-item__name', issueElement).textContent;
         }
 
         let projectName = $$.try('.breadcrumbs__link[data-category]').textContent;
 
         if (!projectName) {
-            let elements = $$.all('.cu-task-view-breadcrumbs__text')
-            let breadcrimsItems = elements.map(x => x.textContent);
-            projectName = breadcrimsItems[breadcrimsItems.length - 2];
+            let elements = $$.all('.cu-task-view-breadcrumbs__v4morelists');
+            let breadcrumbsItems = elements.map(x => x.textContent);
+            projectName = breadcrumbsItems[breadcrumbsItems.length - 1];
         }
 
         if (!projectName) {
